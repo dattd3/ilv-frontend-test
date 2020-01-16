9dd2ccb1b7fccbb8a113925b87ad63209d6dcc5a
Thai Le <v.thaila1@vinpearl.com>
2020-01-16 17:26:27 +0700

    update news

--- src/containers/PersonInfo/index.js
+++ src/containers/PersonInfo/index.js
@@ -4,169 +4,163 @@ import './PersonInfo.css';
 import { useApi, useFetcher } from "../../modules";
 
 const usePreload = (params) => {
-    const api = useApi();
-    const [user = undefined, err] = useFetcher({
-        api: api.fetchPersonCommonInfo,
-        autoRun: true,
-        params: params
-    });
-    return user;
+  const api = useApi();
+  const [user = undefined, err] = useFetcher({
+    api: api.fetchPersonCommonInfo,
+    autoRun: true,
+    params: params
+  });
+  return user;
 };
 
 function PersonInfo() {
 
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    let objDataRes = usePreload([]);
-    if (objDataRes && objDataRes.PersonCommonInfo && objDataRes.CurriculumVitae) {
+  let objDataRes = usePreload([]);
+  if (objDataRes && objDataRes.data) {
 
-     let personCommonInfo = objDataRes.PersonCommonInfo;
-     let curriculumVitae = objDataRes.CurriculumVitae;
+    let personCommonInfo = objDataRes.data.PersonCommonInfo;
+    let curriculumVitae = objDataRes.data.CurriculumVitae;
 
-      return (
-        <div className="bgColor">
-        <br/>
-
-        {/* THÔNG TIN CÁ NHÂN */}
+    return (
+      <div className="bgColor">
+        <br />
 
         <div className='headerText'> {t("PersonalInformation")} </div>
 
         <table>
-        <thead>
-          <tr>
-            <th className="table-header"> {t("FirstAndLastName")} </th>
-            <th className="table-header"> {t("EmployeeCode")} </th>
-            <th className="table-header"> {t("PAndL")} </th>
-            <th className="table-header"> {t("Title")} </th>
-          </tr>
+          <thead>
+            <tr>
+              <th className="table-header"> {t("FirstAndLastName")} </th>
+              <th className="table-header"> {t("EmployeeCode")} </th>
+              <th className="table-header"> {t("PAndL")} </th>
+              <th className="table-header"> {t("Title")} </th>
+            </tr>
           </thead>
           <tbody>
-          <tr>
-            <td className="table-content">
+            <tr>
+              <td className="table-content">
                 <div className="bgText"> {personCommonInfo.FullName} </div>
-            </td>
-            <td >
+              </td>
+              <td >
                 <div className='bgText'> {personCommonInfo.PersonNo} </div>
-            </td>
-            <td>
+              </td>
+              <td>
                 <div className="bgText"> {personCommonInfo.PAndL} </div>
-            </td>
-            <td>
+              </td>
+              <td>
                 <div className="bgText div-end"> {personCommonInfo.Position} </div>
-            </td>
-          </tr>
+              </td>
+            </tr>
           </tbody>
 
-          </table>
-          <br/>
-
-    {/*ĐỊA CHỈ HIỆN TẠI*/}
-    <div className='headerText'> {t("CurrentAddress")} </div>
-      <table>
-            <thead>
-              <tr>
-                <th className="table-header"> {t("Nation")} </th>
-                <th className="table-header"> {t("Province_City")} </th>
-                <th className="table-header"> {t("District")} </th>
-                <th className="table-header"> {t("Address")} </th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr>
-                <td className="table-content">
-                    <div className="bgText"> {curriculumVitae.HT_QuocGia} </div>
-                </td>
-                <td >
-                    <div className='bgText'> {curriculumVitae.HT_ThanhPho} </div>
-                </td>
-                <td >
-                    <div className='bgText'> {curriculumVitae.HT_QuanHuyen} </div>
-                </td>
-                <td>
-                    <div className="bgText div-end"> {curriculumVitae.HT_DiaChi} </div>
-                </td>
-              </tr>
-              </tbody>
-          </table>
-          <br/>
-
-      {/* CMND / THẺ CĂN CƯỚC */}
-      <div className='headerText'> {t("CardId_Title")} </div>
-        <table>
-                  <thead>
-                    <tr>
-                      <th className="table-header"> {t("CardId")} </th>
-                      <th className="table-header"> {t("DateIssue")} </th>
-                      <th className="table-header"> {t("PlaceIssue")} </th>
-                      <th className="table-header"></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                      <td className="table-content">
-                          <div className="bgText"> {curriculumVitae.SoCMND} </div>
-                      </td>
-                      <td >
-                          <div className='bgText'> {curriculumVitae.NgayCapCMND} </div>
-                      </td>
-                      <td>
-                          <div className="bgText div-end"> {curriculumVitae.NoiCapCMND} </div>
-                      </td>
-                    </tr>
-                    </tbody>
         </table>
-        <br/>
+        <br />
 
-        {/* THÔNG TIN TÀI KHOẢN */}
-          <div className='headerText'> {t("AccountInformation")} </div>
-           <table>
-                  <thead>
-                    <tr>
-                      <th className="table-header"> {t("TaxCode")} </th>
-                      <th className="table-header"> {t("SocialInsuranceNumber")} </th>
-                      <th className="table-header"> {t("VinID")} </th>
-                      <th className="table-header"> {t("BankAccountNumber")} </th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                      <td className="table-content">
-                          <div className="bgText"> {curriculumVitae.MaSoThue} </div>
-                      </td>
-                      <td >
-                          <div className='bgText'> {curriculumVitae.SoBHXH} </div>
-                      </td>
-                      <td >
-                          <div className='bgText'> {curriculumVitae.SoTheVinID} </div>
-                      </td>
-                      <td >
-                          <div className='bgText div-end'> {curriculumVitae.SoTaiKhoan} </div>
-                      </td>
-                    </tr>
-                    </tbody>
+        <div className='headerText'> {t("CurrentAddress")} </div>
+        <table>
+          <thead>
+            <tr>
+              <th className="table-header"> {t("Nation")} </th>
+              <th className="table-header"> {t("Province_City")} </th>
+              <th className="table-header"> {t("District")} </th>
+              <th className="table-header"> {t("Address")} </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="table-content">
+                <div className="bgText"> {curriculumVitae.HT_QuocGia} </div>
+              </td>
+              <td >
+                <div className='bgText'> {curriculumVitae.HT_ThanhPho} </div>
+              </td>
+              <td >
+                <div className='bgText'> {curriculumVitae.HT_QuanHuyen} </div>
+              </td>
+              <td>
+                <div className="bgText div-end"> {curriculumVitae.HT_DiaChi} </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <br />
 
-                    <thead>
-                      <tr>
-                        <th className="table-header"> {t("BankName")} </th>
-                      </tr>
-                   </thead>
-                  <tbody>
-                      <tr>
-                          <td className="table-content" colspan="2">
-                              <div className="bgText"> {curriculumVitae.Bank} </div>
-                          </td>
-                      </tr>
-                  </tbody>
-             </table>
-            <br/>
+        <div className='headerText'> {t("CardId_Title")} </div>
+        <table>
+          <thead>
+            <tr>
+              <th className="table-header"> {t("CardId")} </th>
+              <th className="table-header"> {t("DateIssue")} </th>
+              <th className="table-header"> {t("PlaceIssue")} </th>
+              <th className="table-header"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="table-content">
+                <div className="bgText"> {curriculumVitae.SoCMND} </div>
+              </td>
+              <td >
+                <div className='bgText'> {curriculumVitae.NgayCapCMND} </div>
+              </td>
+              <td>
+                <div className="bgText div-end"> {curriculumVitae.NoiCapCMND} </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <br />
 
-        {/* THÔNG TIN NGƯỜI THÂN */}
+        <div className='headerText'> {t("AccountInformation")} </div>
+        <table>
+          <thead>
+            <tr>
+              <th className="table-header"> {t("TaxCode")} </th>
+              <th className="table-header"> {t("SocialInsuranceNumber")} </th>
+              <th className="table-header"> {t("VinID")} </th>
+              <th className="table-header"> {t("BankAccountNumber")} </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="table-content">
+                <div className="bgText"> {curriculumVitae.MaSoThue} </div>
+              </td>
+              <td >
+                <div className='bgText'> {curriculumVitae.SoBHXH} </div>
+              </td>
+              <td >
+                <div className='bgText'> {curriculumVitae.SoTheVinID} </div>
+              </td>
+              <td >
+                <div className='bgText div-end'> {curriculumVitae.SoTaiKhoan} </div>
+              </td>
+            </tr>
+          </tbody>
+
+          <thead>
+            <tr>
+              <th className="table-header"> {t("BankName")} </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="table-content" colspan="2">
+                <div className="bgText"> {curriculumVitae.Bank} </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <br />
+
         <div className='headerText'> {t("LegalInformation")} </div>
 
-         {
+        {
           curriculumVitae.ListFamily.map(function (obj, i) {
-              return (
-           <table className="no-border">
+            return (
+              <table className="no-border">
                 <thead>
                   <tr>
                     <th className="table-header"> {t("FullName")} </th>
@@ -174,35 +168,35 @@ function PersonInfo() {
                     <th className="table-header"> {t("Relationship")} </th>
                     <th className="table-header"> {t("FamilyAllowances")} </th>
                   </tr>
-                  </thead>
-                  <tbody>
+                </thead>
+                <tbody>
                   <tr>
                     <td className="table-content">
-                        <div className="bgText"> {obj.HoVaTen} </div>
+                      <div className="bgText"> {obj.HoVaTen} </div>
                     </td>
                     <td >
-                        <div className='bgText'> {obj.NgaySinh} </div>
+                      <div className='bgText'> {obj.NgaySinh} </div>
                     </td>
                     <td >
-                        <div className='bgText'> {obj.MoiQuanHe} </div>
+                      <div className='bgText'> {obj.MoiQuanHe} </div>
                     </td>
                     <td>
-                        <div className="bgText div-end"> {obj.NgayHieuLucGiamTru} </div>
+                      <div className="bgText div-end"> {obj.NgayHieuLucGiamTru} </div>
                     </td>
                   </tr>
-                  </tbody>
+                </tbody>
               </table>
-              );
-            })
-          }
+            );
+          })
+        }
 
-        <br/>
+        <br />
 
-        </div>
-      );
-    }else {
-      return null;
-    }
+      </div>
+    );
+  } else {
+    return null;
+  }
 
 }
 export default PersonInfo;
