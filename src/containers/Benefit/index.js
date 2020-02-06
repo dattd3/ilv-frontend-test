75ee9424d633eb45881c60cba2a10e39d4f47829
Nguyen Duc Chien <v.chiennd4@vinpearl.com>
2020-02-06 16:53:42 +0700

    Complete show "User Benefit" page with demo data from json file

--- src/containers/Benefit/index.js
+++ src/containers/Benefit/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import BenefitItem from './BenefitItem'
 
+var items = require('./DataTest.json')
+
 function Benefit() {
 
 return (     
-      <div> 
-        <BenefitItem title="Chế độ ưu đãi dịch vụ tại các khách sạn của vinpearl"/>
-        <br/>
+  <div> 
+ {      
+      items.map((item,index) =>  
+         <div key={index}>  
+             <BenefitItem key={index} data={item}/>
+             <br/>
+         </div>
+      )
+  }
+   </div>
+ );
 
-        <BenefitItem title="Chế độ ưu đãi dịch vụ vinmec"/>
-        <br/>
-
-        <BenefitItem title="Chế độ ưu đãi dịch vụ khác"/>
-        <br/>
-
-      </div>
-    );
 }
+
 export default Benefit;
