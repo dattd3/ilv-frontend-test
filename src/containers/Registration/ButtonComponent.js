1732af291875509e7793d0d90c83874857084001
chinh.phungduc <chinh4523@gmail.com>
2021-03-15 17:41:29 +0700

    set multi-lang for registration page(p1)

--- src/containers/Registration/ButtonComponent.js
+++ src/containers/Registration/ButtonComponent.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Spinner from 'react-bootstrap/Spinner'
+import { withTranslation  } from "react-i18next";
 
 class ButtonComponent extends React.Component {
     constructor(props) {
@@ -36,6 +37,7 @@ class ButtonComponent extends React.Component {
     }
 
     render() {
+        const {t} = this.props;
         return <div className="bottom">
             <div className="clearfix mt-5 mb-5">
                 <button type="button" className="btn btn-primary float-right ml-3 shadow" onClick={this.submit.bind(this)} disabled={this.props.disabledSubmitButton}>
@@ -53,7 +55,7 @@ class ButtonComponent extends React.Component {
                             aria-hidden="true"
                             className="mr-2"
                         />}
-                        Gửi yêu cầu
+                        {t('Send')}
                 </button>
                 <input type="file" hidden ref={this.inputReference} id="file-upload" name="file-upload[]" onChange={this.fileUploadInputChange.bind(this)} multiple />
                 <button type="button" className="btn btn-light float-right shadow" onClick={this.fileUploadAction.bind(this)}><i className="fas fa-paperclip"></i> Đính kèm tệp tin</button>
@@ -62,4 +64,4 @@ class ButtonComponent extends React.Component {
     }
 }
 
-export default ButtonComponent
+export default withTranslation()(ButtonComponent)
