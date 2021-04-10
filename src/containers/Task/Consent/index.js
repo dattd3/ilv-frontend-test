dd4171773a8e500f893635a01ca69361a1575638
chinh.phungduc <chinh4523@gmail.com>
2021-04-10 09:08:10 +0700

    fix show list req

--- src/containers/Task/Consent/index.js
+++ src/containers/Task/Consent/index.js
@@ -29,7 +29,7 @@ class ConsentComponent extends React.Component {
       if (res && res.data && res.data.data && res.data.result) {
         const result = res.data.result;
         if (result.code != Constants.API_ERROR_CODE) {
-          let tasksOrdered =res.data.data.requests.sort((a, b) => a.id <= b.id ? 1 : -1)
+          let tasksOrdered =res.data.data.requests
           let taskList = [];
           tasksOrdered.forEach(element => {
             element.requestInfo.forEach(e => {
@@ -37,6 +37,13 @@ class ConsentComponent extends React.Component {
                 e.appraiser = element.appraiser
                 e.requestType = element.requestType
                 e.requestTypeId = element.requestTypeId
+                if(element.requestTypeId == 5 || element.requestTypeId == 4)
+                {
+                  e.timesheets.forEach(ts => {
+                    // e.startDate = ts.startDate
+                    e.id = element.id
+                  })
+                }
                 taskList.push(e);
             })
           });
