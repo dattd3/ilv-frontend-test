ec2fc32c1a02605e6feb337af00c4c935800852f
chinh.phungduc <chinh4523@gmail.com>
2021-04-16 15:16:02 +0700

    fix show detail

--- src/containers/Registration/BusinessTrip/BusinessTripDetailComponent.js
+++ src/containers/Registration/BusinessTrip/BusinessTripDetailComponent.js
@@ -37,8 +37,9 @@ class BusinessTripDetailComponent extends React.Component {
 
   render() {
     const businessTrip = this.props.businessTrip
+    const requestInfo = this.props.businessTrip.requestInfo[0]
     const requestTypeId = this.props.businessTrip.requestTypeId
-    const {t} = this.props
+    const { t } = this.props
     return (
       <div className="business-trip">
         <h5>Thông tin CBNV đăng ký</h5>
@@ -49,97 +50,94 @@ class BusinessTripDetailComponent extends React.Component {
           <div className="row">
             <div className="col-4">
               {t("StartDateTime")}
-              <div className="detail">{moment(businessTrip.requestInfo?.startDate).format('DD/MM/YYYY') + (businessTrip.requestInfo?.startTime ? ' ' + moment(businessTrip.requestInfo?.startTime, TIME_FORMAT).lang('en-us').format('HH:mm') : '')}</div>
+              <div className="detail">{moment(requestInfo?.startDate).format('DD/MM/YYYY') + (requestInfo?.startTime ? ' ' + moment(requestInfo?.startTime, TIME_FORMAT).lang('en-us').format('HH:mm') : '')}</div>
             </div>
             <div className="col-4">
               {t("EndDateTime")}
-              <div className="detail">{moment(businessTrip.requestInfo?.endDate).format('DD/MM/YYYY') + (businessTrip.requestInfo?.endTime ? ' ' + moment(businessTrip.requestInfo?.endTime, TIME_FORMAT).lang('en-us').format('HH:mm') : '')}</div>
+              <div className="detail">{moment(requestInfo?.endDate).format('DD/MM/YYYY') + (requestInfo?.endTime ? ' ' + moment(requestInfo?.endTime, TIME_FORMAT).lang('en-us').format('HH:mm') : '')}</div>
             </div>
             <div className="col-4">
               {t('TotalTimeForBizTripAndTraining')}
-              {/* <div className="detail">{(businessTrip && businessTrip.requestInfo?.hours) ? ((businessTrip.requestInfo.isAllDay == FULL_DAY) ? businessTrip.requestInfo?.days + ' ' + t("Day") : businessTrip.requestInfo?.days + ' ' + t("Day") +' '+businessTrip.requestInfo?.hours + ' ' + t("Hour")) : null}</div> */}
-              <div className="detail">{(businessTrip && businessTrip.requestInfo?.days >=1) ? businessTrip.requestInfo?.days + ' ' + t("Day") : businessTrip.requestInfo?.hours + ' ' + t("Hour")}</div>
+              {/* <div className="detail">{(businessTrip && requestInfo?.hours) ? ((requestInfo.isAllDay == FULL_DAY) ? requestInfo?.days + ' ' + t("Day") : requestInfo?.days + ' ' + t("Day") +' '+requestInfo?.hours + ' ' + t("Hour")) : null}</div> */}
+              <div className="detail">{(businessTrip && requestInfo?.days >=1) ? requestInfo?.days + ' ' + t("Day") : requestInfo?.hours + ' ' + t("Hour")}</div>
             </div>
           </div>
           <div className="row">
             <div className="col-4">
               {t('TypeOfBizTripAndTraining')}
-              <div className="detail">{businessTrip.requestInfo.attendanceType?.label}</div>
+              <div className="detail">{requestInfo.attendanceType?.label}</div>
             </div>
             <div className="col-4">
               {t('Location')}
-              <div className="detail">{businessTrip.requestInfo.location && businessTrip.requestInfo.location?.label}</div>
+              <div className="detail">{requestInfo.location && requestInfo.location?.label}</div>
             </div>
             <div className="col-4">
               {t('MeansOfTransportation')}
-              <div className="detail">{businessTrip.requestInfo.vehicle && businessTrip.requestInfo.vehicle.label}</div>
+              <div className="detail">{requestInfo.vehicle && requestInfo.vehicle.label}</div>
             </div>
           </div>
           <div className="row">
             <div className="col">
               {t('ReasonTripAndTrainning')}
-              <div className="detail">{businessTrip.requestInfo.comment}</div>
+              <div className="detail">{requestInfo.comment}</div>
             </div>
           </div>
         </div>
         {
-          businessTrip.requestInfo && (Constants.STATUS_TO_SHOW_CONSENTER.includes(businessTrip.requestInfo.processStatusId)) ? 
+          requestInfo && (Constants.STATUS_TO_SHOW_CONSENTER.includes(requestInfo.processStatusId)) ? 
           <>
             <h5>Thông tin CBQL thẩm định</h5>
-            <ApproverDetailComponent title={t("Consenter")} approver={businessTrip.appraiser} status={businessTrip.requestInfo ? businessTrip.requestInfo.processStatusId : ""} hrComment={businessTrip.requestInfo.appraiserComment} />
+            <ApproverDetailComponent title={t("Consenter")} approver={businessTrip.appraiser} status={requestInfo ? requestInfo.processStatusId : ""} hrComment={requestInfo.appraiserComment} />
           </>
           : null
         }
         {
-          this.getTypeDetail() === "request" || Constants.STATUS_TO_SHOW_APPROVER.includes(businessTrip.requestInfo.processStatusId)?
+          this.getTypeDetail() === "request" || Constants.STATUS_TO_SHOW_APPROVER.includes(requestInfo.processStatusId)?
           <>
-          <h5>Thông tin phê duyệt</h5>
-          <ApproverDetailComponent title={t("Approver")} approver={businessTrip.approver} status={businessTrip.requestInfo.processStatusId} hrComment={businessTrip.hrComment} />
+            <h5>Thông tin phê duyệt</h5>
+            <ApproverDetailComponent title={t("Approver")} approver={businessTrip.approver} status={requestInfo.processStatusId} hrComment={businessTrip.hrComment} />
           </> : null
         }
 
         {
           businessTrip.requestDocuments.length > 0 ?
           <>
-          <h5>{t("Evidence")}</h5>
-          <ul className="list-inline">
-            {businessTrip.requestDocuments.map((file, index) => {
-              return <li className="list-inline-item" key={index}>
-                <a className="file-name" href={file.fileUrl} title={file.fileName} target="_blank" download={file.fileName}>{file.fileName}</a>
-              </li>
-            })}
-          </ul>
+            <h5>{t("Evidence")}</h5>
+            <ul className="list-inline">
+              {
+                businessTrip.requestDocuments.map((file, index) => {
+                  return (
+                    <li className="list-inline-item" key={index}>
+                      <a className="file-name" href={file.fileUrl} title={file.fileName} target="_blank" download={file.fileName}>{file.fileName}</a>
+                    </li>
+                  )
+                })
+              }
+            </ul>
           </>
           : null
         }
         <div className="block-status">
-          <span className={`status ${Constants.mappingStatus[businessTrip.requestInfo.processStatusId].className}`}>{(this.props.action == "consent" && businessTrip.requestInfo.processStatusId == 5 && businessTrip.appraiser) ? t(Constants.mappingStatus[6].label) : t(Constants.mappingStatus[businessTrip.requestInfo.processStatusId].label)}</span>
+          <span className={`status ${Constants.mappingStatus[requestInfo.processStatusId].className}`}>{(this.props.action == "consent" && requestInfo.processStatusId == 5 && businessTrip.appraiser) ? t(Constants.mappingStatus[6].label) : t(Constants.mappingStatus[requestInfo.processStatusId].label)}</span>
         </div>
-        {(businessTrip.requestInfo.processStatusId === 8 || (this.props.action != "consent" && businessTrip.requestInfo.processStatusId === 5) || businessTrip.requestInfo.processStatusId === 2) ? <DetailButtonComponent 
-        dataToSap={[{
-          // MYVP_ID: 'ATT' + '0'.repeat(9 - businessTrip.id.toString().length) + businessTrip.id,
-          // PERNR: businessTrip.user.employeeNo,
-          // BEGDA: moment(businessTrip.requestInfo.startDate, DATE_FORMAT).format(DATE_OF_SAP_FORMAT),
-          // ENDDA: moment(businessTrip.requestInfo.endDate, DATE_FORMAT).format(DATE_OF_SAP_FORMAT),
-          // SUBTY: businessTrip.requestInfo.attendanceQuotaType.value,
-          // BEGUZ: businessTrip.requestInfo.startTime ? moment(businessTrip.requestInfo.startTime, TIME_FORMAT).format(TIME_OF_SAP_FORMAT) : null,
-          // ENDUZ: businessTrip.requestInfo.endTime ? moment(businessTrip.requestInfo.endTime, TIME_FORMAT).format(TIME_OF_SAP_FORMAT) : null,
-          // ACTIO: 'INS'
-          "id": businessTrip.id,
-          "requestTypeId": Constants.BUSINESS_TRIP,
-          "sub": [
-            {
-              "id": businessTrip.requestInfo.id,
-            }
-          ]
-        }]}
-        isShowRevocationOfApproval={businessTrip.requestInfo.processStatusId === Constants.STATUS_APPROVED}
-        isShowConsent = {businessTrip.requestInfo.processStatusId === Constants.STATUS_WAITING_CONSENTED}
-        isShowRevocationOfConsent = {businessTrip.requestInfo.processStatusId === Constants.STATUS_WAITING && businessTrip.appraiser}
-        id={businessTrip.id}
-        urlName={'requestattendance'}
-        requestTypeId={requestTypeId}
-        action={this.props.action}
+        {(requestInfo.processStatusId === 8 || (this.props.action != "consent" && requestInfo.processStatusId === 5) || requestInfo.processStatusId === 2) ? 
+        <DetailButtonComponent 
+          dataToSap={[{
+            "id": businessTrip.id,
+            "requestTypeId": Constants.BUSINESS_TRIP,
+            "sub": [
+              {
+                "id": requestInfo.id,
+              }
+            ]
+          }]}
+          isShowRevocationOfApproval={requestInfo.processStatusId === Constants.STATUS_APPROVED}
+          isShowConsent = {requestInfo.processStatusId === Constants.STATUS_WAITING_CONSENTED}
+          isShowRevocationOfConsent = {requestInfo.processStatusId === Constants.STATUS_WAITING && businessTrip.appraiser}
+          id={businessTrip.id}
+          urlName={'requestattendance'}
+          requestTypeId={requestTypeId}
+          action={this.props.action}
         /> : null}
       </div>
     )
