31d355e07369af04b6a948150885795a9c937883
chinh.phungduc <chinh4523@gmail.com>
2021-04-16 15:16:31 +0700

    fix show off code

--- src/containers/Registration/Substitution/ShiftTable.js
+++ src/containers/Registration/Substitution/ShiftTable.js
@@ -35,8 +35,8 @@ class ShiftTable extends React.Component {
                                 </th>
                                 <td>{shift.shift_id}</td>
                                 <td>{shift.hours}</td>
-                                <td>{shift.from_time.replace("#",'') ? moment(shift.from_time, TIME_OF_SAP_FORMAT).format(TIME_FORMAT) : null}</td>
-                                <td>{shift.to_time.replace("#",'') ? moment(shift.to_time, TIME_OF_SAP_FORMAT).format(TIME_FORMAT) : null}</td>
+                                <td>{shift.from_time.replace("#", "") ? moment(shift.from_time, TIME_OF_SAP_FORMAT).format(TIME_FORMAT) : ""}</td>
+                                <td>{shift.to_time.replace("#", "") ? moment(shift.to_time, TIME_OF_SAP_FORMAT).format(TIME_FORMAT) : ""}</td>
                             </tr>
                         })}
                     </tbody>
