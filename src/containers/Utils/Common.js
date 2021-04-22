178c997999bda7cbc1016fa07697e1ca75222037
chinh.phungduc <chinh4523@gmail.com>
2021-04-22 10:42:52 +0700

    check reqinfo data

--- src/containers/Utils/Common.js
+++ src/containers/Utils/Common.js
@@ -4,20 +4,22 @@ import Constants from '../../commons/Constants'
 export default function processingDataReq (dataRawFromApi)  {
     let taskList = [];
         dataRawFromApi.forEach(element => {
-            element.requestInfo.forEach(e => {
-                e.user = element.user
-                e.appraiser = element.appraiser
-                e.requestType = element.requestType
-                e.requestTypeId = element.requestTypeId
-                e.startDate = moment(e.startDate).format("DD/MM/YYYY")
-                if(element.requestTypeId == Constants.IN_OUT_TIME_UPDATE || element.requestTypeId == Constants.SUBSTITUTION)
-                {
-                  e.processStatusId = element.processStatusId
-                  e.id = element.id.toString()
-                  e.startDate = moment(e.date).format("DD/MM/YYYY")
-                }
-                taskList.push(e);
-            })
+            if(element.requestInfo) {
+                element.requestInfo.forEach(e => {
+                    e.user = element.user
+                    e.appraiser = element.appraiser
+                    e.requestType = element.requestType
+                    e.requestTypeId = element.requestTypeId
+                    e.startDate = moment(e.startDate).format("DD/MM/YYYY")
+                    if(element.requestTypeId == Constants.IN_OUT_TIME_UPDATE || element.requestTypeId == Constants.SUBSTITUTION)
+                    {
+                      e.processStatusId = element.processStatusId
+                      e.id = element.id.toString()
+                      e.startDate = moment(e.date).format("DD/MM/YYYY")
+                    }
+                    taskList.push(e);
+                })
+            }
           });
           taskList =  taskList.filter(function(e, index ,b) {
             var k = b.map(z => z.id)
