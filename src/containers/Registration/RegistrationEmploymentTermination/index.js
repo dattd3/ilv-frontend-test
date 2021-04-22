be07b84bbf1b5da925e2e86a145f2388dc21ad6d
Nguyễn Văn Cường (VST-KPTSP-PPTSP) <cuongnv56@vingroup.net>
2021-04-22 16:36:13 +0700

    Create UIs for Registration Employment Termination functions

---
+++ src/containers/Registration/RegistrationEmploymentTermination/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Tabs, Tab } from 'react-bootstrap'
+import LeaveOfAbsence from './LeaveOfAbsence/LeaveOfAbsenceComponent'
+import BusinessTrip from './BusinessTrip/BusinessTripComponent'
+import SubstitutionComponent from './Substitution/SubstitutionComponent'
+import InOutTimeUpdate from './InOutTimeUpdate/InOutTimeUpdateComponent'
+import { withTranslation  } from "react-i18next";
+class RegistrationComponent extends React.Component {
+  constructor(props) {
+    super()
+    this.state = {
+      tab: new URLSearchParams(props.history.location.search).get('tab') || "LeaveOfAbsenceRegistration",
+    }
+  }
+
+  updateTabLink = key => {
+    this.props.history.push('?tab=' + key);
+    this.setState({ tab: key })
+  }
+
+  render() {
+    const { t } = this.props;
+    return (
+      <div className="registration-section personal-info justify-content-between">
+        <Tabs defaultActiveKey={this.state.tab} onSelect={(key) => this.updateTabLink(key)}>
+          <Tab eventKey="LeaveOfAbsenceRegistration" title={t('LeaveRequest')}>
+            <LeaveOfAbsence />
+          </Tab>
+          {/* <Tab eventKey="BusinessTripRegistration" title={t('BizTrip_TrainingRequest')}>
+            <BusinessTrip />
+          </Tab>
+          { 
+            !['V096','V073'].includes(localStorage.getItem("companyCode")) ?
+           <Tab eventKey="SubstitutionRegistration" title={t('ShiftChange')}>
+            <SubstitutionComponent />
+          </Tab> : null
+          }
+          <Tab eventKey="InOutTimeUpdate" title={localStorage.getItem("companyCode") != "V096" ? t('InOutChangeRequest') : t('Explanation')}>
+            <InOutTimeUpdate />
+          </Tab> */}
+        </Tabs>
+      </div>
+    )
+  }
+}
+export default withTranslation()(RegistrationComponent)
