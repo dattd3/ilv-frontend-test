0f08fdcdf8f74867ee087fc57c5384b9ab5b7f87
chinh.phungduc <chinh4523@gmail.com>
2021-04-23 10:26:04 +0700

    add revocation Approval function

--- src/containers/Registration/DetailButtonComponent.js
+++ src/containers/Registration/DetailButtonComponent.js
@@ -33,12 +33,12 @@ class DetailButtonComponent extends React.Component {
 
     revocationApproval = () => {
         const { t } = this.props
-        this.setState({ isConfirmShow: true, modalTitle: t("ConfirmApprovalRecall"), modalMessage: t("SureApprovalRecall") + t(this.requestRegistraion[this.props.requestTypeId]), typeRequest: Constants.STATUS_REVOCATION })
+        this.setState({ isConfirmShow: true, modalTitle: t("ConfirmApprovalRecall"), modalMessage: t("SureApprovalRecall") + t(this.requestRegistraion[this.props.requestTypeId]), typeRequest: 0 })
     }
 
     evictionRequest = () => {
         const { t } = this.props
-        this.setState({ isConfirmShow: true, modalTitle: t("ConfirmRequestRecall"), modalMessage: t("SureRequestRecall") + t(this.requestRegistraion[this.props.requestTypeId]) + " này ?", typeRequest: Constants.STATUS_EVICTION })
+        this.setState({ isConfirmShow: true, modalTitle: t("ConfirmRequestRecall"), modalMessage: t("SureRequestRecall") + t(this.requestRegistraion[this.props.requestTypeId]) + " này ?", typeRequest: 0 })
     }
     consent = () => {
         const { t } = this.props
@@ -97,11 +97,11 @@ class DetailButtonComponent extends React.Component {
                     </>
                     : null
                 }
-                {/* {
+                {
                     this.props.isShowRevocationOfApproval && !this.props.hiddenRevocationOfApprovalButton ?
                     <button type="button" className="btn btn-danger float-right shadow" onClick={this.revocationApproval.bind(this)}><i className='fas fa-undo-alt'></i> {t("ApproralRecall")}</button>
                     : null
-                } */}
+                }
             </div>
             : null
             }
@@ -125,13 +125,13 @@ class DetailButtonComponent extends React.Component {
             </div>
             : null
             }
-            {
+            {/* {
                 action === "request" && this.props.isShowEvictionRequest ?
                 <div className="clearfix mt-5 mb-5">
                     <button type="button" className="btn btn-danger float-right shadow" onClick={this.evictionRequest.bind(this)}><i className='fas fa-undo-alt'></i> Thu hồi yêu cầu</button>
                 </div>
                 : null
-            }
+            } */}
         </div>
     }
 }
