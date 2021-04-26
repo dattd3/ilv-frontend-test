2ecc8a8e550695951885f3899cf73b34e1cb6893
chinh.phungduc <chinh4523@gmail.com>
2021-04-26 09:47:18 +0700

    fix height box

--- src/containers/Registration/ApproverDetailComponent.js
+++ src/containers/Registration/ApproverDetailComponent.js
@@ -5,22 +5,22 @@ import { useTranslation } from "react-i18next";
 function ApproverDetailComponent(props) {
   const { t } = useTranslation();
     return <div className="box shadow cbnv">
-    <div className="row">
-      <div className="col-xl-4">
+    <div className="row group">
+      <div className="col-xl-4 auto-height">
         {props.title}
         <div className="detail">{props.approver ? props.approver.fullName : null}</div>
       </div>
-      <div className="col-xl-4">
+      <div className="col-xl-4 auto-height">
         {t('Position')}
         <div className="detail">{props.approver ? props.approver.current_position : null}</div>
       </div>
-      <div className="col-xl-4">
+      <div className="col-xl-4 auto-height">
         {t('DepartmentManage')}
         <div className="detail">{props.approver ? props.approver.department : null}</div>
       </div>
       {
       props.status == Constants.STATUS_NOT_APPROVED || props.status == Constants.STATUS_NO_CONSENTED || props.status == Constants.STATUS_EVICTION ?
-      <div className="col-xl-4">
+      <div className="col-xl-4 auto-height">
         Lý do không duyệt
         <div className="detail">
           <span className="hr-comments">{props.hrComment ? props.hrComment : ""}</span>
