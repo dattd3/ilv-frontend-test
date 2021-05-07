eefc945dc67d9ae5dce6a729ea15d9374e7a5fe5
Nguyễn Văn Cường (VST-KPTSP-PPTSP) <cuongnv56@vingroup.net>
2021-05-07 17:46:20 +0700

    Submit data for Propose to resign employees functions and Implement Termination interview functions

--- src/containers/Registration/RegistrationEmploymentTermination/ContractTerminationInterview.js
+++ src/containers/Registration/RegistrationEmploymentTermination/ContractTerminationInterview.js
@@ -1,27 +1,29 @@
 import React from 'react'
 import axios from 'axios'
-import DatePicker, { registerLocale } from 'react-datepicker'
 import moment from 'moment'
 import { Image } from 'react-bootstrap';
 import _ from 'lodash'
 import { withTranslation } from "react-i18next"
 import Constants from '../../../commons/Constants'
-import ButtonComponent from '../ButtonComponent'
+import ButtonComponent from '../TerminationComponents/ButtonComponent'
 import StaffInfoForContractTerminationInterviewComponent from '../TerminationComponents/StaffInfoForContractTerminationInterviewComponent'
 import InterviewContentFormComponent from '../TerminationComponents/InterviewContentFormComponent'
 import ResultModal from '../ResultModal'
 import VinpearlLogo from '../../../assets/img/logo-vp-vt.png'
 
-import 'react-datepicker/dist/react-datepicker.css'
-import { vi, enUS } from 'date-fns/locale'
+const config = {
+    headers: {            
+        'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
+    }
+}
 
 class ContractTerminationInterview extends React.Component {
     constructor(props) {
-        super();
+        super(props)
         this.state = {
-            approver: null,
-            appraiser: null,
-            annualLeaveSummary: null,
+            timeJoinDefault: null,
+            timeInDefault: null,
+            serveyInfos: {},
             files: [],
             isUpdateFiles: false,
             isEdit: false,
@@ -33,19 +35,126 @@ class ContractTerminationInterview extends React.Component {
         }
     }
 
-    static getDerivedStateFromProps(nextProps, prevState) {
-        const { leaveOfAbsence } = nextProps
-        if (leaveOfAbsence) {
-            return ({
-                approver: leaveOfAbsence.approver,
-                appraiser: leaveOfAbsence.appraiser
-            })
-        }
-        return prevState
-    }
+    // static getDerivedStateFromProps(nextProps, prevState) {
+    //     const { leaveOfAbsence } = nextProps
+    //     if (leaveOfAbsence) {
+    //         return ({
+    //             approver: leaveOfAbsence.approver,
+    //             appraiser: leaveOfAbsence.appraiser
+    //         })
+    //     }
+    //     return prevState
+    // }
 
     componentDidMount() {
-       
+        this.initialData()
+    }
+
+    initialData = async () => {
+        const serveyInfosEndpoint = `${process.env.REACT_APP_REQUEST_URL}getworkoffserveyinfo`
+        const userInfosEndpoint = `${process.env.REACT_APP_MULE_HOST}api/sap/hcm/v1/ws/user/profile`
+        const userContractInfosEndpoint = `${process.env.REACT_APP_MULE_HOST}api/sap/hcm/v1/ws/user/contract`
+        const requestServeyInfos = axios.get(serveyInfosEndpoint, config)
+        const requestUserInfos = axios.get(userInfosEndpoint, config)
+        const requestUserContractInfos = axios.get(userContractInfosEndpoint, config)
+
+        await axios.all([requestServeyInfos, requestUserInfos, requestUserContractInfos]).then(axios.spread((...responses) => {
+            const serveyInfos = this.prepareServeyInfos(responses[0])
+            const userInfos = this.prepareUserInfos(responses[1], responses[2])
+
+            this.setState({userInfos: userInfos, serveyInfos: serveyInfos})
+        })).catch(errors => {
+            return null
+        })
+    }
+
+    prepareServeyInfos = serveyResponses => {
+        if (serveyResponses && serveyResponses.data) {
+            const servey = serveyResponses.data.data
+            const items = servey.workOffServeyItemModel
+
+            const categoryMapping = {
+                1: {code: "CURRENT_JOB", label: "Công việc hiện tại"},
+                2: {code: "MANAGER", label: "Quản lý"},
+                3: {code: "SALARY_BONUS_REMUNERATION", label: "Lương thưởng & Chế độ đãi ngộ"},
+                4: {code: "PERSONAL_REASONS", label: "Lý do cá nhân"}
+            }
+
+            console.log("kkkkkkkkkkk")
+            console.log(items)
+
+            const CURRENT_JOB = 1
+            const MANAGER = 2
+            const SALARY_BONUS_REMUNERATION = 3
+            const PERSONAL_REASONS = 4
+
+            const currentJobItems = (items || [])
+            .filter(item => item.type == CURRENT_JOB)
+            .map(item => {
+                return {
+                    value: item.id,
+                    label: item.name
+                }
+            })
+
+            const managerItems = (items || [])
+            .filter(item => item.type == MANAGER)
+            .map(item => {
+                return {
+                    value: item.id,
+                    label: item.name
+                }
+            })
+
+
+
+            // [
+            //     {
+            //         code: "CURRENT_JOB",
+            //         data: [
+            //             {
+            //                 value: 1,
+            //                 label: "Địa điểm làm việc"
+            //             }
+            //         ]
+            //     }
+            // ]
+        }
+    }
+
+    prepareUserInfos = (userResponses, contractResponses) => {
+        let userInfoResults = {}
+        let userContractInfoResults = {}
+        if (userResponses && userResponses.data) {
+            const userInfos = userResponses.data.data
+            if (userInfos && userInfos.length > 0) {
+                const infos = userInfos[0]
+                const dateStartWork = infos && infos.starting_date_co && moment(infos.starting_date_co, "DD-MM-YYYY").isValid() ? moment(infos.starting_date_co, "DD-MM-YYYY").format("YYYY-MM-DD") : ""
+                userInfoResults = {
+                    employeeNo: localStorage.getItem("employeeNo") || "",
+                    fullName: infos.fullname || "",
+                    jobTitle: infos.job_name || "",
+                    department: `${infos.division || ""}${infos.department ? `/${infos.department}` : ""}${infos.part ? `/${infos.part}` : ""}`,
+                    dateStartWork: dateStartWork,
+                    email: localStorage.getItem("email") || "",
+                    rank: infos.rank_name || "",
+                    unitName: infos.unit || ""
+                }
+            }
+        }
+
+        if (contractResponses && contractResponses.data) {
+            const contractInfos = contractResponses.data.data
+            if (contractInfos && contractInfos.length > 0) {
+                const infos = contractInfos[0]
+                userContractInfoResults = {
+                    contractType: infos.contract_number,
+                    contractName: infos.contract_type || ""
+                }
+            }
+        }
+
+        return {...userInfoResults, ...userContractInfoResults}
     }
 
     showStatusModal = (title, message, isSuccess = false) => {
@@ -53,13 +162,17 @@ class ContractTerminationInterview extends React.Component {
     }
 
     hideStatusModal = () => {
-        const { isEdit } = this.state;
-        this.setState({ isShowStatusModal: false });
+        const { isEdit } = this.state
+        this.setState({ isShowStatusModal: false })
         if (isEdit) {
             window.location.replace("/tasks")
-        } else {
-            window.location.reload();
         }
+
+        window.location.reload()
+    }
+
+    updateInterviewContents = (type, value) => {
+        this.setState({[type]: value})
     }
 
     render() {
@@ -69,6 +182,8 @@ class ContractTerminationInterview extends React.Component {
         const messageModal = ""
         const isSuccess = true
 
+        const {userInfos, serveyInfos} = this.state
+
         return (
             <>
             <ResultModal show={isShowStatusModal} title={titleModal} message={messageModal} isSuccess={isSuccess} onHide={this.hideStatusModal} />
@@ -77,8 +192,8 @@ class ContractTerminationInterview extends React.Component {
                     <Image src={VinpearlLogo} alt="Vinpearl" className="logo" />
                 </div>
                 <h5 className="page-title">Biểu mẫu phỏng vấn thôi việc</h5>
-                <StaffInfoForContractTerminationInterviewComponent />
-                <InterviewContentFormComponent />
+                <StaffInfoForContractTerminationInterviewComponent userInfos={userInfos} />
+                <InterviewContentFormComponent updateInterviewContents={this.updateInterviewContents} />
                 <ButtonComponent />
             </div>
             </>
--- src/containers/Registration/TerminationComponents/ReasonResignationComponent.js
+++ src/containers/Registration/TerminationComponents/ReasonResignationComponent.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import Select from 'react-select'
-import { withTranslation } from "react-i18next";
+import DatePicker from 'react-datepicker'
+import moment from 'moment'
+import { withTranslation } from "react-i18next"
+import 'react-datepicker/dist/react-datepicker.css'
+import { vi, enUS } from 'date-fns/locale'
 
 class ReasonResignationComponent extends React.PureComponent {
     constructor(props) {
@@ -17,6 +21,20 @@ class ReasonResignationComponent extends React.PureComponent {
             this.setState({infos: infos})
             this.props.updateResignationReasons(infos)
         }
+    }
+
+    handleDatePickerChange = date => {
+        const infos = {...this.state.infos}
+        infos.lastWorkingDay = null
+        infos.dateTermination = null
+
+        if (moment(date, 'YYYY-MM-DD').isValid()) {
+            infos.lastWorkingDay = moment(date).format('YYYY-MM-DD')
+            infos.dateTermination = moment(date, 'YYYY-MM-DD').add(1, 'days').format('YYYY-MM-DD')
+        }
+
+        this.setState({infos: infos})
+        this.props.updateResignationReasons(infos)
     }
 
     handleInputChange = e => {
@@ -45,17 +63,42 @@ class ReasonResignationComponent extends React.PureComponent {
         return <div className="block reason-resignation-block">
                     <h6 className="block-title">II. Lý do CBLĐ TT đề xuất cho nghỉ</h6>
                     <div className="box shadow">
-                        <div className="row">
+                    <div className="row">
+                            <div className="col-4">
+                                <p className="title">{t('LastWorkingDay')}</p>
+                                <div className="content input-container">
+                                    <label>
+                                        <DatePicker
+                                            selectsStart
+                                            autoComplete="off"
+                                            selected={infos.lastWorkingDay ? moment(infos.lastWorkingDay, "YYYY-MM-DD").toDate() : null}
+                                            onChange={date => this.handleDatePickerChange(date)}
+                                            dateFormat="dd/MM/yyyy"
+                                            placeholderText={t('Select')}
+                                            locale={t("locale")}
+                                            className="form-control input" />
+                                        <span className="input-group-addon input-img"><i className="fas fa-calendar-alt text-info"></i></span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div className="col-4">
+                                <p className="title">{t('ContractTerminationDate')}</p>
+                                <div>
+                                    <input type="text" className="form-control" value={infos.dateTermination ? moment(infos.dateTermination, "YYYY-MM-DD").format('DD/MM/YYYY') : ""} readOnly />
+                                </div>
+                            </div>
                             <div className="col-4">
                                 <p className="title">{t('ReasonForContractTermination')}</p>
                                 <div>
                                     <Select options={reasonTypes} placeholder="Vui lòng chọn lý do" onChange={this.handleSelectChange} value={infos.reason} styles={customStyles} />
                                 </div>
                             </div>
-                            <div className="col-8">
+                        </div>
+                        <div className="row">
+                            <div className="col-12">
                                 <p className="title">{t('DetailedReason')}</p>
                                 <div>
-                                    <input type="text" placeholder="Nhập lý do chi tiết" className="form-control" value={infos.reasonDetailed || ""} onChange={this.handleInputChange} />
+                                    <input type="text" className="form-control" value={infos.reasonDetailed || ""} onChange={this.handleInputChange} />
                                 </div>
                             </div>
                         </div>
