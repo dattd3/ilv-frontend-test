58e0dc887d5cb3dfafae9a6b8df407201832222b
Nguyễn Văn Cường (VIN3S-KPMĐG-PPTPMK) <cuongnv56@vingroup.net>
2021-05-17 16:06:24 +0700

    Add paging for Resignation request management page

--- src/containers/Registration/RegistrationEmploymentTermination/ResignationRequestsManagementPage.js
+++ src/containers/Registration/RegistrationEmploymentTermination/ResignationRequestsManagementPage.js
@@ -9,6 +9,7 @@ import Constants from '../../../commons/Constants'
 import ResignationRequestsManagementActionButton from '../TerminationComponents/ResignationRequestsManagementActionButton'
 import ListStaffResignationComponent from '../TerminationComponents/ListStaffResignationComponent'
 import ResultModal from '../ResultModal'
+import CustomPaging from '../../../components/Common/CustomPaging'
 import "react-toastify/dist/ReactToastify.css"
 
 const config = {
@@ -30,6 +31,7 @@ class ResignationRequestsManagementPage extends React.Component {
         super();
         this.state = {
             listUserTerminations: [],
+            totalUserTerminations: 0,
             searchingDataToFilter: {
                 pageIndex: Constants.PAGE_INDEX_DEFAULT,
                 pageSize: Constants.PAGE_SIZE_DEFAULT,
@@ -81,7 +83,7 @@ class ResignationRequestsManagementPage extends React.Component {
         config.params = params
         const responses = await axios.get(`${process.env.REACT_APP_REQUEST_URL}ReasonType/getlistterminal`, config)
         const listUserTerminations = this.prepareListUserTerminations(responses)
-        this.setState({listUserTerminations: listUserTerminations, isShowLoadingModal: false})
+        this.setState({listUserTerminations: listUserTerminations.data, totalUserTerminations: listUserTerminations.total, isShowLoadingModal: false})
     }
 
     prepareParamsToFilter = () => {
@@ -89,56 +91,27 @@ class ResignationRequestsManagementPage extends React.Component {
     }
 
     prepareListUserTerminations = responses => {
-        if (responses && responses.data && responses.data.data && responses.data.data.data) {
+        if (responses && responses.data && responses.data.data) {
+            const total = responses.data.data.totalRecord
             const userTerminations = responses.data.data.data
+
             if (userTerminations && userTerminations.length > 0) {
-                return userTerminations
+                return {
+                    data: userTerminations,
+                    total: total
+                }
             }
 
-            return []
+            return {
+                data: [],
+                total: 0
+            }
         }
 
-        return []
-    }
-
-    verifyInput() {
-        // let { requestInfo, approver, appraiser, errors } = this.state;
-        // requestInfo.forEach((req, indexReq) => {
-        //     if (!req.startDate) {
-        //         req.errors["startDate"] = this.props.t('Required')
-        //     }
-        //     if (!req.endDate) {
-        //         req.errors["endDate"] = this.props.t('Required')
-        //     }
-        //     if (!req.startTime && !req.isAllDay && !req.isAllDayCheckbox) {
-        //         req.errors["startTime"] = this.props.t('Required')
-        //     }
-        //     if (!req.endTime && !req.isAllDay && !req.isAllDayCheckbox) {
-        //         req.errors["endTime"] = this.props.t('Required')
-        //     }
-        //     if (!req.absenceType) {
-        //         requestInfo[indexReq].errors.absenceType = this.props.t('Required')
-        //     }
-        //     if (!req.comment) {
-        //         requestInfo[indexReq].errors.comment = this.props.t('Required')
-        //     }
-        //     requestInfo[indexReq].errors['pn03'] = (requestInfo[indexReq].absenceType && requestInfo[indexReq].absenceType.value === 'PN03' && _.isNull(requestInfo[indexReq]['pn03'])) ? this.props.t('Required') : null
-        // })
-        // const employeeLevel = localStorage.getItem("employeeLevel")
-
-        // this.setState({
-        //     requestInfo,
-        //     errors: {
-        //         approver: !approver ? this.props.t('Required') : errors.approver,
-        //         // appraiser: !appraiser && employeeLevel === "N0" ? this.props.t('Required') : errors.appraiser
-        //     }
-        // })
-
-        // const listError = requestInfo.map(req => _.compact(_.valuesIn(req.errors))).flat()
-        // if (listError.length > 0 || errors.approver) { //|| (errors.appraiser && employeeLevel === "N0")
-        //     return false
-        // }
-        // return true
+        return {
+            data: [],
+            total: 0
+        }
     }
 
     isNullCustomize = value => {
@@ -264,17 +237,14 @@ class ResignationRequestsManagementPage extends React.Component {
                 requestObj = this.getRequestConfig(typeMethodMapping[type], `${process.env.REACT_APP_REQUEST_URL}WorkOffServey/exportToExcel`)
                 break
             case LIQUIDATION_AGREEMENT:
-                // TODO
                 requestConfig = this.getRequestConfig(typeMethodMapping[type], `${process.env.REACT_APP_REQUEST_URL}Template/exportword_bienban_thanhly_hopdong`)
                 requestObj = {...requestConfig, ...{data: {ids: requestHistoryIds}}}
                 break
             case CONTRACT_TERMINATION_AGREEMENT:
-                // TODO
                 requestConfig = this.getRequestConfig(typeMethodMapping[type], `${process.env.REACT_APP_REQUEST_URL}Template/exportword_thoathuan_chamdut_hopdong`)
                 requestObj = {...requestConfig, ...{data: {ids: requestHistoryIds}}}
                 break
             case DECISION_CONTRACT_TERMINATION:
-                // TODO
                 requestConfig = this.getRequestConfig(typeMethodMapping[type], `${process.env.REACT_APP_REQUEST_URL}Template/exportword_quyetdinh_chamdut_hopdong`)
                 requestObj = {...requestConfig, ...{data: {ids: requestHistoryIds}}}
                 break
@@ -317,15 +287,12 @@ class ResignationRequestsManagementPage extends React.Component {
                 this.handleExportResponses(responses, "Bao-cao-ket-qua-phong-van.xlsx")
                 break
             case LIQUIDATION_AGREEMENT:
-                // TODO
                 this.handleExportResponses(responses, "Bien-ban-thanh-ly.zip")
                 break
             case CONTRACT_TERMINATION_AGREEMENT:
-                // TODO
                 this.handleExportResponses(responses, "Thoa-thuan-cham-dut-hop-dong.zip")
                 break
             case DECISION_CONTRACT_TERMINATION:
-                // TODO
                 this.handleExportResponses(responses, "Quyet-dinh-cham-dut-hop-dong.zip")
                 break
         }
@@ -460,10 +427,19 @@ class ResignationRequestsManagementPage extends React.Component {
         return false
     }
 
+    onChangePage = page => {
+        const searchingDataToFilter = this.state.searchingDataToFilter
+        searchingDataToFilter.pageIndex = page
+        this.setState({searchingDataToFilter: searchingDataToFilter}, () => {
+            this.fetchListUserTerminations()
+        })
+    }
+
     render() {
         const { t } = this.props
         const {
             listUserTerminations,
+            totalUserTerminations,
             isEdit,
             errors,
             titleModal,
@@ -471,7 +447,8 @@ class ResignationRequestsManagementPage extends React.Component {
             disabledSubmitButton,
             isShowStatusModal,
             isSuccess,
-            isShowLoadingModal
+            isShowLoadingModal,
+            searchingDataToFilter
         } = this.state
 
         return (
@@ -486,6 +463,7 @@ class ResignationRequestsManagementPage extends React.Component {
                 <ResignationRequestsManagementActionButton updateKeywordsToFilter={this.updateKeywordsToFilter} updateAttachedFiles={this.updateAttachedFiles} save={this.save} updateOptionToExport={this.updateOptionToExport} />
                 <ListStaffResignationComponent listUserTerminations={listUserTerminations} updateTerminationRequestList={this.updateTerminationRequestList} />
             </div>
+            <CustomPaging pageSize={searchingDataToFilter.pageSize} onChangePage={this.onChangePage} totalRecords={totalUserTerminations} />
             </>
         )
     }
