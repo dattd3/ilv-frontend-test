aee77df48a680a8b051c7cade05cc55d87a63b17
chinhpd1802 <chinh4523@gmail.com>
2021-07-26 11:54:34 +0700

    fix bug translate and format code

--- src/containers/QuestionAndAnswer/HistoryTable.js
+++ src/containers/QuestionAndAnswer/HistoryTable.js
@@ -1,81 +1,134 @@
 import React, { useState } from "react";
-import { Modal, Image, Form, Button } from 'react-bootstrap';
-import { withTranslation } from "react-i18next"
-
+import { Button, Tooltip, OverlayTrigger } from "react-bootstrap";
+import { withTranslation } from "react-i18next";
 class HistoryTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isShowConfirmModal: false,
-      questionSelectedID: 0
-    }
+      questionSelectedID: 0,
+    };
   }
 
   generateStatus(questionStatus) {
     switch (questionStatus) {
       case 2:
-        return <span className="align-self-center question-status inprogress">{this.props.t("Replied")}</span>
+        return (
+          <span className="align-self-center question-status inprogress">
+            {this.props.t("Replied")}
+          </span>
+        );
       case 1:
-        return <span className="align-self-center question-status cancelled">{this.props.t("Waiting")}</span>
+        return (
+          <span className="align-self-center question-status cancelled">
+            {this.props.t("Processing")}
+          </span>
+        );
       case 3:
-        return <span className="align-self-center question-status overdue">Quá hạn</span>;
+        return (
+          <span className="align-self-center question-status overdue">
+            Quá hạn
+          </span>
+        );
       default:
         return <span className="course-status undefined">Undefined</span>;
     }
   }
   showConfirmModal(selectedId) {
-    this.props.showConfirms(true,selectedId);
+    this.props.showConfirms(true, selectedId);
   }
-  showEditModal(question){
+
+  showEditModal(question) {
     this.props.showEditModal(question);
   }
+  
   render() {
     const { t } = this.props;
-    return <div className="wrap-result text-left">
-      { this.props.questions && this.props.questions.length > 0 ?
-      this.props.questions.map((question, index) => {
-        return <div key={index}>
-          <div className="media">
-            <span className="img-circle align-self-center">
-              <i className="icon icon-qa-unselected align-self-center"></i>
-            </span>
-            <div className="media-body row">
-              <div className="col-sm-12 col-lg-8">
-                <h5 className="mt-1"><a href={`/question-and-answer-details/` + question.id}>{question.content}</a></h5>
-                <span>
-                  <i className="icon-hr mr-1"></i> {question.fullName}
-                  <i className="fa fa-clock-o mr-1 ml-3"></i>{question.createdAt}
-                </span>
-              </div>
+    return (
+      <div className="wrap-result text-left">
+        {this.props.questions && this.props.questions.length > 0 ? (
+          this.props.questions.map((question, index) => {
+            return (
+              <div key={index}>
+                <div className="media">
+                  <span className="img-circle align-self-center">
+                    <i className="icon icon-qa-unselected align-self-center"></i>
+                  </span>
+                  <div className="media-body row">
+                    <div className="col-sm-12 col-lg-8">
+                      <h5 className="mt-1">
+                        <a href={`/question-and-answer-details/` + question.id}>
+                          {question.content}
+                        </a>
+                      </h5>
+                      <span>
+                        <i className="icon-hr mr-1"></i> {question.fullName}
+                        <i className="fa fa-clock-o mr-1 ml-3"></i>
+                        {question.createdAt}
+                      </span>
+                    </div>
 
-              <div className="col-sm-12 col-lg-4 pull-right content-center">
-                <div className="col-7 content-center">
-                  {this.generateStatus(question.ticketStatusId)}
+                    <div className="col-sm-12 col-lg-4 pull-right content-center">
+                      <div className="col-7 content-center">
+                        {this.generateStatus(question.ticketStatusId)}
+                      </div>
+                      <div className=" col-5 content-center text-left">
+                        {question &&
+                        question.ticketStatusId === 1 &&
+                        question.userId === localStorage.getItem("email") ? (
+                          <>
+                            <OverlayTrigger
+                              key="edit-tooltip"
+                              placement="bottom"
+                              overlay={
+                                <Tooltip id="edit-tooltip">
+                                  {t("EditQuestion")}
+                                </Tooltip>
+                              }
+                            >
+                              <Button
+                                variant="outline-light content-center p-0 ml-3 border-0"
+                                onClick={() => this.showEditModal(question)}
+                              >
+                                <i className="icon icon-qa-edit align-self-center"></i>
+                              </Button>
+                            </OverlayTrigger>
+                            <OverlayTrigger
+                              key="edit-tooltip"
+                              placement="bottom"
+                              overlay={
+                                <Tooltip id="edit-tooltip">
+                                  {t("Withdraw")}
+                                </Tooltip>
+                              }
+                            >
+                              <Button
+                                variant="outline-light content-center p-0 pt-0 ml-3 border-0"
+                                onClick={() =>
+                                  this.showConfirmModal(question.id)
+                                }
+                              >
+                                <i className="icon icon-qa-undo align-self-center"></i>
+                              </Button>
+                            </OverlayTrigger>
+                          </>
+                        ) : null}
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div className=" col-5 content-center text-left">
-                  {
-                    (question && question.ticketStatusId === 1 && question.userId === localStorage.getItem('email')) ?
-                      <>
-                        <Button variant="outline-light content-center p-0 ml-3 border-0" onClick={() => this.showEditModal(question)} >
-                          <i className="icon icon-qa-edit align-self-center"></i>
-                        </Button>
-                        <Button variant="outline-light content-center p-0 pt-0 ml-3 border-0" onClick={() => this.showConfirmModal(question.id)}>
-                          <i className="icon icon-qa-undo align-self-center"></i>
-                        </Button>
-                      </>
-                      : null
-                  }
-                </div>
+                <hr />
               </div>
-            </div>
+            );
+          })
+        ) : (
+          <div className="text-center mt-5">
+            <p>{t("NoDataFound")}!</p>
           </div>
-          <hr />
-        </div>
-      }) :
-      <div className="text-center mt-5"><p>{t("NoDataFound")}!</p></div>
-      }
-    </div>
+        )}
+      </div>
+    );
   }
 }
 
-export default withTranslation()(HistoryTable)
+export default withTranslation()(HistoryTable);
