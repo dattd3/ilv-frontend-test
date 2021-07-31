f9e1d99640955d488251730e9bd1e7656fb2a377
Nguyễn Văn Cường (VIN3S-KPMĐG-PPTPMK) <cuongnv56@vingroup.net>
2021-07-31 15:51:31 +0700

    Update list tasks for childcare leave

--- src/containers/Utils/Common.js
+++ src/containers/Utils/Common.js
@@ -31,11 +31,23 @@ export default function processingDataReq(dataRawFromApi, tab) {
             })
         }
     });
+
     taskList = taskList.filter(function (e, index, b) {
         var k = b.map(z => z.id)
         if (k.includes(e.id, index + 1)) {
             var indexPosition = k.indexOf(e.id, index + 1);
             b[indexPosition].startDate = (e.startDate + ",\r" + b[indexPosition].startDate);
+        } else if (e.absenceType && e.absenceType.value == "PN02") {
+            const startDate = moment(e.startDate, "DD/MM/YYYY")
+            const endDate = moment(e.endDate, "YYYYMMDD")
+            let now = startDate, dates = []
+
+            while (now.isBefore(endDate) || now.isSame(endDate)) {
+                dates.push(now.format('DD/MM/YYYY'));
+                now.add(1, 'days')
+            }
+            e.startDate = dates.join(",\r")
+            return e;
         } else {
             e.startDate = e.startDate;
             return e;
