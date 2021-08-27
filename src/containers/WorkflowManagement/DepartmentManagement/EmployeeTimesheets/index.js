ba69ecd282af05a75c7b9cc36d3c2bad147f071f
VINGROUP\datth3 <v.datth3@.vinsoftware.vn>
2021-08-27 21:31:40 +0700

    Skip check is_holiday in BCC for Vinmec

--- src/containers/WorkflowManagement/DepartmentManagement/EmployeeTimesheets/index.js
+++ src/containers/WorkflowManagement/DepartmentManagement/EmployeeTimesheets/index.js
@@ -141,7 +141,7 @@ class EmployeeTimesheets extends Component {
   };
 
   isHoliday = (item) => {
-    return item.shift_id == "OFF" || item.is_holiday == 1;
+    return item.shift_id == 'OFF' || (item.is_holiday == 1 && localStorage.getItem("companyCode") != "V060");
   };
 
   getDatetimeForCheckFail(startTime, endTime, currentDay, nextDay) {
