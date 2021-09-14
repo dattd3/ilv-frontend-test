fa8d972c16397da856ba232e53993e8634864c1c
chinhpd1802 <chinh4523@gmail.com>
2021-09-14 10:37:08 +0700

    fix check empty user and show error login

--- src/containers/Login/authozire.js
+++ src/containers/Login/authozire.js
@@ -38,24 +38,31 @@ function Authorize(props) {
 
         axios.get(`${process.env.REACT_APP_MULE_HOST}api/sap/hcm/v1/ws/user/profile`, config)
             .then(res => {
-                if (res && res.data && res.data.data) {
+                if (res && res.data && res.data.data[0]) {
                     let userProfile = res.data.data[0];
                     checkUser(userProfile, jwtToken, vgEmail);
                     updateUser(userProfile,jwtToken)
                 }
+                else {
+                    SetNotifyContent(t("NotFoundUserOnSap"));
+                    SetIsloading(false);
+                    SetIsLoadingUser(false);
+                    SetIsGetUser(true)
+                }
             })
             .catch(error => {
-                console.log("Call getUser error:", error);
+                // console.log("Call getUser error:", error);
                 SetNotifyContent(t("LoginError"));
                 SetIsloading(false);
                 SetIsLoadingUser(false);
+                SetIsGetUser(true)
             }
             );
     }
 
     const checkUser = (user, jwtToken, vgEmail) => {
         if (user == null || user.uid == null) {
-            SetNotifyContent(t("LoginError"));
+            SetNotifyContent(t("NotFoundUserOnSap"));
             SetIsloading(false);
         } else {
             SetNotifyContent(t("WaitNotice"));
