57b30f5eacc92c926c5ab68ac84987dbd919fcb6
Nguyễn Văn Cường (VIN3S-KPMĐG-PPTPMK) <cuongnv56@vingroup.net>
2021-11-05 20:21:57 +0700

    Update Home page UI

--- src/containers/Dashboard/NewsOnHome.js
+++ src/containers/Dashboard/NewsOnHome.js
@@ -1,23 +1,28 @@
-import React from "react";
-import { Card, ListGroup } from 'react-bootstrap';
-import { useApi, useFetcher } from "../../modules";
-import { useTranslation } from "react-i18next";
-import LoadingSpinner from "../../components/Forms/CustomForm/LoadingSpinner";
-import moment from 'moment';
+import React, { useState } from "react"
+import { Image } from 'react-bootstrap'
+import { useTranslation } from "react-i18next"
+import moment from 'moment'
+import { useApi, useFetcher } from "../../modules"
+
+// import LoadingSpinner from "../../components/Forms/CustomForm/LoadingSpinner"
+import CustomPaging from '../../components/Common/CustomPaging'
+import IconDiamond from '../../assets/img/icon/Icon-Diamond.svg'
+import IconViewDetail from '../../assets/img/icon/Icon-Arrow-Right.svg'
+import IconUser from '../../assets/img/icon/Icon-User.svg'
+import IconTime from '../../assets/img/icon/Icon-Time.svg'
 
 const usePreload = (params) => {
     const api = useApi();
-    const [listArticles = undefined, err] = useFetcher({
-        api: api.fetchNewsOnHome,
+    const [data = [], err] = useFetcher({
+        api: api.fetchArticleList,
         autoRun: true,
         params: params
     });
-    return listArticles;
+    return data;
 };
 
-function convertToSlug(input) {
-    var slug = input.toLowerCase();
-
+const convertToSlug = input => {
+    let slug = input?.toLowerCase()
     slug = slug.replace(/á|à|ả|ạ|ã|ă|ắ|ằ|ẳ|ẵ|ặ|â|ấ|ầ|ẩ|ẫ|ậ/gi, 'a')
         .replace(/é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ/gi, 'e')
         .replace(/i|í|ì|ỉ|ĩ|ị/gi, 'i')
@@ -34,53 +39,220 @@ function convertToSlug(input) {
         .replace(/[^\w\-]+/g, '')
         .replace(/\-\-+/g, '-')
         .replace(/\@\-|\-\@|\@/gi, '');
-    return slug;
+    return slug
 }
 
-function SubStringDescription(input) {
-    if (input.length > 150) {
-        return input.substr(0, 149);
-    } else {
-        return input;
+const subStringDescription = input => {
+    return input && input?.length > 150 ? input.substr(0, 149) : input
+}
+
+const getTimeByRawTime = rawTime => {
+    const time = moment(rawTime).isValid() ? moment(rawTime) : null
+    return {
+        time: time?.format("HH:mm") || "",
+        date: time?.format("DD/MM/YYYY") || ""
     }
 }
 
 function NewsOnHome(props) {
+    const { t } = useTranslation()
+    const [pageIndex, SetPageIndex] = useState(1)
+    const [pageSize, SetPageSize] = useState(9)
 
-    const { t } = useTranslation();
-    const listArticles = usePreload();
+    const listArticles = usePreload([pageIndex, pageSize])
+
+
+    const articles = listArticles?.data || []
+    const totalArticles = articles.totalRecord
+
+    console.log("AAAAAAAAAAAAAAAAAAAA")
+    console.log(articles)
+
+
+    const onChangePage = page => {
+        SetPageIndex(page)
+    }
+
+    const topOne = totalArticles > 0 ? articles.listArticles[0] : null
+    const timePublishedTopOne = getTimeByRawTime(topOne?.publishedDate)
+
+    console.log(topOne?.thumbnail)
+
     return (
-        <Card className="mb-4 news-home shadow">
-            <Card.Body className="card-body pd-0">
-                <ListGroup variant="flush">
-                    <ListGroup.Item>
-                        <span className="db-card-header color-pink"><i className="fas icon-groupnotice"></i> {t("Menu_GroupInformation")}</span>
-                    </ListGroup.Item>
-                    {
-                        listArticles && listArticles.data ?
-                            listArticles.data.map(function (obj, i) {
-                                return (
+        totalArticles > 0 ?
+        <>
+        <h1 className="page-title"><Image src={IconDiamond} alt="News" className="ic-page-title" />{t("NewsAndEvent")}</h1>
+        <div className="top-news">
+            <div className="row">
+                <div className="col-md-6 special">
+                    <div className="top-one">
+                        <a href={`/news/${convertToSlug(topOne.title)}/${topOne.id}`} title={topOne.title} className="link-detail">
+                            <Image src={topOne?.thumbnail} alt="News" className="thumbnail" 
+                                onError={(e) => {
+                                    e.target.src = "https://ircdn.vingroup.net/storage/Uploads/0_Tintuchoatdong/2021/Jun/Anh%20VinFuture%20Prize%20Council.jpg"
+                                }}
+                            />
+                            <p className="title">{topOne.title || ""}</p>
+                        </a>
+                        <div className="other-info">
+                            <div className="source-time-info">
+                                <span className="source"><Image src={IconUser} alt="Source" className="icon" /><span className="source-name">{topOne.sourceSite || ""}</span></span>
+                                <span className="time"><Image src={IconTime} alt="Time" className="icon" /><span className="hour">{timePublishedTopOne.time} | {timePublishedTopOne.date}</span></span>
+                            </div>
+                            <p className="description">{subStringDescription(topOne.description)}...</p>
+                            <div className="btn-detail">
+                                <a href={`/news/${convertToSlug(topOne.title)}/${topOne.id}`} title={topOne.title} className="detail"><span>{t("Details")}</span><Image src={IconViewDetail} alt="Detail" className="icon-view-detail" /></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6 other">
+                    <div className="top-four">
+                        <div className="item">
+                            <a href={`/news/${convertToSlug(topOne.title)}/${topOne.id}`} title={topOne.title} className="link-image-detail">
+                                <Image src={topOne.thumbnail} alt="News" className="thumbnail" />
+                            </a>
+                            <div className="title-source-time-info">
+                                <a href={`/news/${convertToSlug(topOne.title)}/${topOne.id}`} title={topOne.title} className="title">{topOne.title}</a>
+                                <div className="source-time-info">
+                                    <span className="source"><Image src={IconUser} alt="Source" className="icon" /><span className="source-name">{topOne.sourceSite || ""}</span></span>
+                                    <span className="time"><Image src={IconTime} alt="Time" className="icon" /><span className="hour">{timePublishedTopOne.time} | {timePublishedTopOne.date}</span></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <a href={`/news/${convertToSlug(topOne.title)}/${topOne.id}`} title={topOne.title} className="link-image-detail">
+                                <Image src={topOne.thumbnail} alt="News" className="thumbnail" />
+                            </a>
+                            <div className="title-source-time-info">
+                                <a href={`/news/${convertToSlug(topOne.title)}/${topOne.id}`} title={topOne.title} className="title">{topOne.title}</a>
+                                <div className="source-time-info">
+                                    <span className="source"><Image src={IconUser} alt="Source" className="icon" /><span className="source-name">{topOne.sourceSite || ""}</span></span>
+                                    <span className="time"><Image src={IconTime} alt="Time" className="icon" /><span className="hour">{timePublishedTopOne.time} | {timePublishedTopOne.date}</span></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <a href={`/news/${convertToSlug(topOne.title)}/${topOne.id}`} title={topOne.title} className="link-image-detail">
+                                <Image src={topOne.thumbnail} alt="News" className="thumbnail" />
+                            </a>
+                            <div className="title-source-time-info">
+                                <a href={`/news/${convertToSlug(topOne.title)}/${topOne.id}`} title={topOne.title} className="title">{topOne.title}</a>
+                                <div className="source-time-info">
+                                    <span className="source"><Image src={IconUser} alt="Source" className="icon" /><span className="source-name">{topOne.sourceSite || ""}</span></span>
+                                    <span className="time"><Image src={IconTime} alt="Time" className="icon" /><span className="hour">{timePublishedTopOne.time} | {timePublishedTopOne.date}</span></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <a href={`/news/${convertToSlug(topOne.title)}/${topOne.id}`} title={topOne.title} className="link-image-detail">
+                                <Image src={topOne.thumbnail} alt="News" className="thumbnail" />
+                            </a>
+                            <div className="title-source-time-info">
+                                <a href={`/news/${convertToSlug(topOne.title)}/${topOne.id}`} title={topOne.title} className="title">{topOne.title}</a>
+                                <div className="source-time-info">
+                                    <span className="source"><Image src={IconUser} alt="Source" className="icon" /><span className="source-name">{topOne.sourceSite || ""}</span></span>
+                                    <span className="time"><Image src={IconTime} alt="Time" className="icon" /><span className="hour">{timePublishedTopOne.time} | {timePublishedTopOne.date}</span></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="other-news">
+            <div className="row">
+                <div className="col-md-12">
+                    <div className="item">
+                        <a href={`/news/${convertToSlug(topOne.title)}/${topOne.id}`} title={topOne.title} className="link-image-detail">
+                            <Image src={topOne.thumbnail} alt="News" className="thumbnail" />
+                        </a>
+                        <div className="title-source-time-info">
+                            <div className="main-info">
+                                <a href={`/news/${convertToSlug(topOne.title)}/${topOne.id}`} title={topOne.title} className="title">{topOne.title}</a>
+                                <p className="description">{subStringDescription(topOne.description)}...</p>
+                                <div className="source-time-info">
+                                    <span className="source"><Image src={IconUser} alt="Source" className="icon" /><span className="source-name">{topOne.sourceSite || ""}</span></span>
+                                    <span className="time"><Image src={IconTime} alt="Time" className="icon" /><span className="hour">{timePublishedTopOne.time} | {timePublishedTopOne.date}</span></span>
+                                </div>
+                            </div>
+                            <div className="btn-detail">
+                                <a href={`/news/${convertToSlug(topOne.title)}/${topOne.id}`} title={topOne.title} className="detail"><span>{t("Details")}</span><Image src={IconViewDetail} alt="Detail" className="icon-view-detail" /></a>
+                            </div>
+                        </div>
+                    </div>
 
-                                    <ListGroup.Item key={i} className="news-home-item">
-                                        <a href={`/news/${convertToSlug(obj.title)}/${obj.id}`}>
-                                            <div className="nhome-thumbnail" style={{ backgroundImage: `url(${obj.thumbnail})` }}></div>
-                                        </a>
-                                        <div className="nhome-infor">
-                                            <a href={`/news/${convertToSlug(obj.title)}/${obj.id}`}>
-                                                <Card.Title>{obj.title}</Card.Title>
-                                            </a>
-                                            <Card.Text className="small text-gray-600">{SubStringDescription(obj.description)}...</Card.Text>
-                                            <span className="small"><i className="far fa-clock"></i> {moment(obj.publishedDate).format('DD/MM/YYYY')} - {obj.sourceSite}</span>
-                                        </div>
-                                    </ListGroup.Item>
+                    <div className="item">
+                        <a href={`/news/${convertToSlug(topOne.title)}/${topOne.id}`} title={topOne.title} className="link-image-detail">
+                            <Image src={topOne.thumbnail} alt="News" className="thumbnail" />
+                        </a>
+                        <div className="title-source-time-info">
+                            <div className="main-info">
+                                <a href={`/news/${convertToSlug(topOne.title)}/${topOne.id}`} title={topOne.title} className="title">{topOne.title}</a>
+                                <p className="description">{subStringDescription(topOne.description)}...</p>
+                                <div className="source-time-info">
+                                    <span className="source"><Image src={IconUser} alt="Source" className="icon" /><span className="source-name">{topOne.sourceSite || ""}</span></span>
+                                    <span className="time"><Image src={IconTime} alt="Time" className="icon" /><span className="hour">{timePublishedTopOne.time} | {timePublishedTopOne.date}</span></span>
+                                </div>
+                            </div>
+                            <div className="btn-detail">
+                                <a href={`/news/${convertToSlug(topOne.title)}/${topOne.id}`} title={topOne.title} className="detail"><span>{t("Details")}</span><Image src={IconViewDetail} alt="Detail" className="icon-view-detail" /></a>
+                            </div>
+                        </div>
+                    </div>
 
-                                );
-                            })
-                            : <ListGroup.Item><LoadingSpinner /></ListGroup.Item>
-                    }
-                </ListGroup>
-            </Card.Body>
-        </Card>
+                    <div className="item">
+                        <a href={`/news/${convertToSlug(topOne.title)}/${topOne.id}`} title={topOne.title} className="link-image-detail">
+                            <Image src={topOne.thumbnail} alt="News" className="thumbnail" />
+                        </a>
+                        <div className="title-source-time-info">
+                            <div className="main-info">
+                                <a href={`/news/${convertToSlug(topOne.title)}/${topOne.id}`} title={topOne.title} className="title">{topOne.title}</a>
+                                <p className="description">{subStringDescription(topOne.description)}...</p>
+                                <div className="source-time-info">
+                                    <span className="source"><Image src={IconUser} alt="Source" className="icon" /><span className="source-name">{topOne.sourceSite || ""}</span></span>
+                                    <span className="time"><Image src={IconTime} alt="Time" className="icon" /><span className="hour">{timePublishedTopOne.time} | {timePublishedTopOne.date}</span></span>
+                                </div>
+                            </div>
+                            <div className="btn-detail">
+                                <a href={`/news/${convertToSlug(topOne.title)}/${topOne.id}`} title={topOne.title} className="detail"><span>{t("Details")}</span><Image src={IconViewDetail} alt="Detail" className="icon-view-detail" /></a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="item">
+                        <a href={`/news/${convertToSlug(topOne.title)}/${topOne.id}`} title={topOne.title} className="link-image-detail">
+                            <Image src={topOne.thumbnail} alt="News" className="thumbnail" />
+                        </a>
+                        <div className="title-source-time-info">
+                            <div className="main-info">
+                                <a href={`/news/${convertToSlug(topOne.title)}/${topOne.id}`} title={topOne.title} className="title">{topOne.title}</a>
+                                <p className="description">{subStringDescription(topOne.description)}...</p>
+                                <div className="source-time-info">
+                                    <span className="source"><Image src={IconUser} alt="Source" className="icon" /><span className="source-name">{topOne.sourceSite || ""}</span></span>
+                                    <span className="time"><Image src={IconTime} alt="Time" className="icon" /><span className="hour">{timePublishedTopOne.time} | {timePublishedTopOne.date}</span></span>
+                                </div>
+                            </div>
+                            <div className="btn-detail">
+                                <a href={`/news/${convertToSlug(topOne.title)}/${topOne.id}`} title={topOne.title} className="detail"><span>{t("Details")}</span><Image src={IconViewDetail} alt="Detail" className="icon-view-detail" /></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="row">
+            <div className="col-sm"></div>
+            <div className="col-sm">
+                <CustomPaging pageSize={parseInt(pageSize)} onChangePage={onChangePage} totalRecords={totalArticles} />
+            </div>
+            <div className="col-sm text-right">
+                {t("Total")}: {totalArticles}
+            </div>
+        </div>
+        </>
+        : t("DataNotFound")
     );
 }
 
