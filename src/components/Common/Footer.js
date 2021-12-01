81649018fdbeae7f4c1c83a6e4e7e31767d511e0
khanhnn17 <v.khanhnn17@vin3s.vn>
2021-12-01 09:01:04 +0700

    load lazy dash board

--- src/components/Common/Footer.js
+++ src/components/Common/Footer.js
@@ -1,14 +1,15 @@
 import React from "react";
 
 function Footer(props) {
-    return (
-        <footer style={{backgroundColor: localStorage.getItem("companyThemeColor")}} className="sticky-footer bg-whit shadow vg-footer mb-3">
-        <div className="container my-auto">
-          <div className="copyright text-center my-auto h4">
-            <span>Copyright &copy; 2021 <b className="">Vingroup</b>. Designed by <b className="">Vin3S</b> All Rights Reserved</span>
-          </div>
+  const { isDashBoard } = props;
+  return (
+    <footer style={{ backgroundColor: localStorage.getItem("companyThemeColor") }} className={`sticky-footer bg-whit shadow ${isDashBoard === true ? 'vg-dard-board' : 'vg-footer'} mb-3`}>
+      <div className="container my-auto">
+        <div className="copyright text-center my-auto h4">
+          <span>Copyright &copy; 2021 <b className="">Vingroup</b>. Designed by <b className="">Vin3S</b> All Rights Reserved</span>
         </div>
-      </footer>
-    );
+      </div>
+    </footer>
+  );
 }
 export default Footer;
