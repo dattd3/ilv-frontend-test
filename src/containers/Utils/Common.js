4e03674aecc69133ddf0c4abf633a595d1952e11
VINGROUP\datth3 <v.datth3@vin3s.vn>
2022-02-24 11:20:39 +0700

    check requesttype = 6 trong processingDataReq contract onboarding

--- src/containers/Utils/Common.js
+++ src/containers/Utils/Common.js
@@ -21,56 +21,60 @@ export default function processingDataReq(dataRawFromApi, tab) {
     const listRequestTypeIdToShowTime = [Constants.LEAVE_OF_ABSENCE, Constants.BUSINESS_TRIP, Constants.SUBSTITUTION, Constants.IN_OUT_TIME_UPDATE]
     const listRequestTypeIdToGetSubId = [Constants.LEAVE_OF_ABSENCE, Constants.BUSINESS_TRIP]
     dataRawFromApi.forEach(element => {
-        if (element.requestInfo) {
-            element.requestInfo.forEach(e => {
-                e.user = element.user
-                e.appraiser = element.appraiser
-                e.appraiserId = element.appraiserId
-                e.requestType = element.requestType
-                e.requestTypeId = element.requestTypeId
-
-                if (element.requestTypeId == Constants.UPDATE_PROFILE) {
-                    e.processStatusId = element.processStatusId
-                    e.comment = element.comment;
-                    e.approverComment = element.approverComment;
-                }
-                if (listRequestTypeIdToShowTime.includes(element.requestTypeId)) {
-                    let date = [moment(e.date).format("DD/MM/YYYY")]
-                    if (element.requestTypeId == Constants.SUBSTITUTION) {
-                        if (!e?.applyFrom && !e?.applyTo) {
-                            date = [moment(e?.date, 'YYYYMMDD').format("DD/MM/YYYY")]
-                        } else if (e?.applyFrom === e?.applyTo) {
-                            date = [moment(e?.applyFrom, 'YYYYMMDD').format("DD/MM/YYYY")]
-                        } else {
-                            date = getDateByRange(e?.applyFrom, e?.applyTo)
-                        }
-                    } else if (element.requestTypeId == Constants.LEAVE_OF_ABSENCE || element.requestTypeId == Constants.BUSINESS_TRIP) {
-                        if (e?.startDate && e?.startDate === e?.endDate) {
-                            date = [moment(e?.startDate, 'YYYYMMDD').format("DD/MM/YYYY")]
-                        } else {
-                            date = getDateByRange(e?.startDate, e?.endDate)
-                        }
-                    }
-                    e.processStatusId = listRequestTypeIdToGetSubId.includes(element.requestTypeId) ? e.processStatusId : element.processStatusId
-                    e.startDate = date
-                    e.comment = element.comment;
-                    e.approverComment = element.approverComment;
-                }
-                if (e.processStatusId == 8 || (e.processStatusId == 5 && tab == "approval")) {
-                    e.canChecked = true
-                }
-                if (listRequestTypeIdToGetSubId.includes(element.requestTypeId)) {
-                    e.id = e.id.toString()
-                } else {
-                    e.id = element.id.toString()
-                }
-                e.isEdit = listRequestTypeIdToGetSubId.includes(element.requestTypeId) ? e.isEdit : element.isEdit
-                taskList.push(e)
-            })
-        }
-        if (element.requestTypeId == Constants.CHANGE_DIVISON_SHIFT || element.requestTypeId == Constants.DEPARTMENT_TIMESHEET) {
-            element.id = element.id.toString()
+        if(element.requestTypeId == 6) {
             taskList.push(element);
+        } else {
+            if (element.requestInfo) {
+                element.requestInfo.forEach(e => {
+                    e.user = element.user
+                    e.appraiser = element.appraiser
+                    e.appraiserId = element.appraiserId
+                    e.requestType = element.requestType
+                    e.requestTypeId = element.requestTypeId
+
+                    if (element.requestTypeId == Constants.UPDATE_PROFILE) {
+                        e.processStatusId = element.processStatusId
+                        e.comment = element.comment;
+                        e.approverComment = element.approverComment;
+                    }
+                    if (listRequestTypeIdToShowTime.includes(element.requestTypeId)) {
+                        let date = [moment(e.date).format("DD/MM/YYYY")]
+                        if (element.requestTypeId == Constants.SUBSTITUTION) {
+                            if (!e?.applyFrom && !e?.applyTo) {
+                                date = [moment(e?.date, 'YYYYMMDD').format("DD/MM/YYYY")]
+                            } else if (e?.applyFrom === e?.applyTo) {
+                                date = [moment(e?.applyFrom, 'YYYYMMDD').format("DD/MM/YYYY")]
+                            } else {
+                                date = getDateByRange(e?.applyFrom, e?.applyTo)
+                            }
+                        } else if (element.requestTypeId == Constants.LEAVE_OF_ABSENCE || element.requestTypeId == Constants.BUSINESS_TRIP) {
+                            if (e?.startDate && e?.startDate === e?.endDate) {
+                                date = [moment(e?.startDate, 'YYYYMMDD').format("DD/MM/YYYY")]
+                            } else {
+                                date = getDateByRange(e?.startDate, e?.endDate)
+                            }
+                        }
+                        e.processStatusId = listRequestTypeIdToGetSubId.includes(element.requestTypeId) ? e.processStatusId : element.processStatusId
+                        e.startDate = date
+                        e.comment = element.comment;
+                        e.approverComment = element.approverComment;
+                    }
+                    if (e.processStatusId == 8 || (e.processStatusId == 5 && tab == "approval")) {
+                        e.canChecked = true
+                    }
+                    if (listRequestTypeIdToGetSubId.includes(element.requestTypeId)) {
+                        e.id = e.id.toString()
+                    } else {
+                        e.id = element.id.toString()
+                    }
+                    e.isEdit = listRequestTypeIdToGetSubId.includes(element.requestTypeId) ? e.isEdit : element.isEdit
+                    taskList.push(e)
+                })
+            }
+            if (element.requestTypeId == Constants.CHANGE_DIVISON_SHIFT || element.requestTypeId == Constants.DEPARTMENT_TIMESHEET) {
+                element.id = element.id.toString()
+                taskList.push(element);
+            }
         }
     });
 
