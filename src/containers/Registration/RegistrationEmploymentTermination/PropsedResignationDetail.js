adb686b70da06776b080b0d5f8c01a1d2c0d7f05
VINGROUP\datth3 <v.datth3@vin3s.vn>
2022-04-15 09:07:58 +0700

    [resign] fix bug force close

--- src/containers/Registration/RegistrationEmploymentTermination/PropsedResignationDetail.js
+++ src/containers/Registration/RegistrationEmploymentTermination/PropsedResignationDetail.js
@@ -217,7 +217,7 @@ class RegistrationEmploymentTermination extends React.Component {
         <AttachmentComponent files={files} updateFiles={this.updateFiles} />
 
         <div className="block-status">
-          <span className={`status ${Constants.mappingStatus[requestInfo.processStatusId].className}`}>{(this.props.action == "consent" && requestInfo.processStatusId == 5 && resignInfo.appraiser) ? t(Constants.mappingStatus[6].label) : t(Constants.mappingStatus[requestInfo.processStatusId].label)}</span>
+          <span className={`status ${Constants.mappingStatusRequest[requestInfo.processStatusId].className}`}>{(this.props.action == "consent" && requestInfo.processStatusId == 5 && resignInfo.appraiser) ? t(Constants.mappingStatusRequest[6].label) : t(Constants.mappingStatusRequest[requestInfo.processStatusId].label)}</span>
         </div>
         {(requestInfo.processStatusId === 8 || (this.props.action != "consent" && requestInfo.processStatusId === 5) || requestInfo.processStatusId === 2) ? 
         <DetailButtonComponent 
