820a2386e4385869b989cc07df6a10e05f47107d
Nguyễn Văn Cường (VIN3S-CPR-Dev) <cuongnv56@vingroup.net>
2022-04-27 17:27:52 +0700

    Binding data to list my evaluation and finish UI evaluation form details

--- src/containers/Evaluation/MyEvaluation/index.js
+++ src/containers/Evaluation/MyEvaluation/index.js
@@ -2,132 +2,151 @@ import React, { useState, useEffect } from "react"
 import Select from 'react-select'
 import { Image } from 'react-bootstrap'
 import { useTranslation } from "react-i18next"
+import moment from 'moment'
 import axios from 'axios'
 import Constants from '../../../commons/Constants'
-// import { status, myProjectPageKey, ILoveVinGroupSite } from '../Constants'
-// import { getRequestConfigurations } from '../../../commons/Utils'
-// import ProjectRowItem from '../Share/ProjectRowItem'
-
+import { useGuardStore } from '../../../modules'
+import { getRequestConfigurations } from '../../../commons/Utils'
+import LoadingModal from '../../../components/Common/LoadingModal'
+import map from '../../map.config'
 import IconLoop from '../../../assets/img/icon/Icon_Loop.svg'
 
-import LoadingModal from '../../../components/Common/LoadingModal'
-import CustomPaging from '../../../components/Common/CustomPaging'
-import map from '../../map.config'
+function EvaluationFormItem(props) {
+    const { item } = props
+    const statusDone = 5
+    const status = item?.status === statusDone ? 'Đã hoàn thành' : 'Đang đánh giá'
+    const stepMapping = {
+        1: 'Khởi tạo',
+        2: 'Khởi chạy',
+        3: 'Tự đánh giá',
+        4: 'QLTT đánh giá',
+        5: 'CBLĐ phê duyệt',
+    }
+
+    return (
+        <tr>
+            <td className='c-form-name'><div className='form-name'>{item?.checkPhaseFormName}</div></td>
+            <td className='c-created-date text-center'><div className='created-date'>{item?.createDate && moment(item?.createDate).format("DD/MM/YYYY")}</div></td>
+            <td className='c-status text-center'><div className='status'>{status}</div></td>
+            <td className='c-step text-center'><div className='step'>{stepMapping[item?.status]}</div></td>
+        </tr>
+    )
+}
 
 function MyEvaluation(props) {
+    const currentYear = new Date().getFullYear()
     const { t } = useTranslation()
-    // const [projectData, SetProjectData] = useState({projects: [], totalRecord: 0})
-    const [paging, SetPaging] = useState({pageIndex: 1, pageSize: 10})
+    const guard = useGuardStore()
+    const user = guard.getCurentUser()
+    const [evaluationForms, SetEvaluationForms] = useState([])
+    const [years, SetYears] = useState([])
+    const [year, SetYear] = useState({value: currentYear, label: currentYear} )
     const [isLoading, SetIsLoading] = useState(false)
-    
+
     useEffect(() => {
-        // if (props.isSetBackUrl === false) {
-        //     return
-        // } else {
-        //     localStorage.setItem('backUrl', window.location.href)
-        // }
+        const processInitialData = response => {
+            const [yearData, evaluationFormData] = response
+
+            if (yearData && yearData?.status === 'fulfilled') {
+                const yearDataValue = yearData?.value
+                if (yearDataValue && yearDataValue?.data && yearDataValue?.data?.result && yearDataValue?.data?.result?.code == Constants.PMS_API_SUCCESS_CODE) {
+                    let yearsResult = yearDataValue?.data?.data
+                    if (!yearsResult.includes(currentYear)) {
+                        yearsResult.push(currentYear)
+                    }
+                    const yearsToSave = (yearsResult || []).map(item => ({value: item, label: item}))
+                    SetYears(yearsToSave)
+                }
+            }
+            if (evaluationFormData && evaluationFormData?.status === 'fulfilled') {
+                const evaluationFormDataValue = evaluationFormData?.value
+                if (evaluationFormDataValue && evaluationFormDataValue?.data && evaluationFormDataValue?.data?.result && evaluationFormDataValue?.data?.result?.code == Constants.PMS_API_SUCCESS_CODE) {
+                    SetEvaluationForms(evaluationFormDataValue?.data?.data || [])
+                }
+            }
+            SetIsLoading(false)
+        }
+
+        const fetchDataInitial = async () => {
+            SetIsLoading(true)
+            try {
+                const config = getRequestConfigurations()
+                const getEvaluationYearConfig = {...config}
+                getEvaluationYearConfig.params = {
+                    EmployeeCode: user?.employeeNo
+                }
+                const requestGetEvaluationYears = axios.get(`${process.env.REACT_APP_HRDX_URL}api/form/listYear`, getEvaluationYearConfig)
+                const requestGetListEvaluationForms = fetchListEvaluationForms()
+                const response = await Promise.allSettled([requestGetEvaluationYears, requestGetListEvaluationForms])
+                processInitialData(response)
+            } catch (e) {
+                console.log(e)
+                SetIsLoading(false)
+            }
+        }
+
+        fetchDataInitial()
     }, [])
 
-    useEffect(() => {
-        // const processProjectData = response => {
-        //     if (response && response.data) {
-        //         const result = response.data.result
-        //         if (result && result.code == Constants.API_SUCCESS_CODE) {
-        //             const data = response.data?.data
-        //             const projectDataToSave = {...projectData}
-        //             projectDataToSave.projects = data?.details || []
-        //             projectDataToSave.totalRecord = data?.totalRecord || 0
-        //             SetProjectData(projectDataToSave)
-        //         }
-        //     }
-        //     SetIsLoading(false)
-        // }
-
-        // const getProjectData = async () => {
-        //     SetIsLoading(true)
-        //     try {
-        //         const config = getRequestConfigurations()
-        //         config.params = {
-        //             pageIndex: paging.pageIndex,
-        //             pageSize: paging.pageSize,
-        //             site: ILoveVinGroupSite
-        //         }
-        //         if (props.from === myProjectPageKey) {
-        //             delete config.params.site
-        //         }
-        //         const apiUrl = props.from === myProjectPageKey ? 'projects/me' : 'projects/list'
-        //         const response = await axios.get(`${process.env.REACT_APP_RSM_URL}${apiUrl}`, config)
-        //         processProjectData(response)
-        //     } catch (e) {
-        //         console.error(e)
-        //         SetIsLoading(false)
-        //     }
-        // }
-
-        // getProjectData()
-    }, [paging])
-
-    const handleStatusClick = (projectId, statusId) => {
-        // window.location.replace(`/project/${projectId}`)
-    }
-
-    const onChangePage = (page) => {
-        // const pagingTemp = {...paging}
-        // pagingTemp.pageIndex = page
-        // SetPaging({...paging, pageIndex: page})
-    }
-
-    const renderListProjects = () => {
-        // return (
-        //     (projectData.projects || []).map((item, index) => {
-        //         return <React.Fragment key={index}>
-        //             <ProjectRowItem item={item} index={index + 1} handleStatusClick={handleStatusClick} />
-        //         </React.Fragment>
-        //     })
-        // )
-    }
-
     const handleChangeSelectInput = e => {
-
+        SetYear(e)
     }
 
-    const handleOnSubmit = e => {}
-
-    const projectData = {
-        projects: []
+    const fetchListEvaluationForms = () => {
+        const config = getRequestConfigurations()
+        config.params = {
+            PageIndex: 1,
+            PageSize: 100,
+            EmployeeCode: user?.employeeNo,
+            Year: year?.value
+        }
+        return axios.get(`${process.env.REACT_APP_HRDX_URL}api/form/listUseForm`, config)
     }
 
-    const yearForFilter = [
-        {value: 2021, label: '2021'},
-        {value: 2022, label: '2022'},
-        {value: 2023, label: '2023'}
-    ]
+    const handleOnSubmit = async (e) => {
+        e.preventDefault()
+        SetIsLoading(true)
+        const response = await fetchListEvaluationForms()
+        if (response && response?.data && response?.data?.result && response?.data?.result?.code == Constants.PMS_API_SUCCESS_CODE) {
+            SetEvaluationForms(response?.data?.data || [])
+            SetIsLoading(false)
+        }
+    }
+
+    const renderListEvaluationForms = () => {
+        return (
+            (evaluationForms || []).map((item, index) => {
+                return <React.Fragment key={index}>
+                    <EvaluationFormItem item={item} />
+                </React.Fragment>
+            })
+        )
+    }
 
     return (
         <>
         <LoadingModal show={isLoading} />
         <div className="my-evaluation-page">
             <h1 className="content-page-header">Đánh giá</h1>
-            {
-                projectData.projects.length !== 0   
-                ? <h6 className="alert alert-danger" role="alert">{t("NoDataFound")}</h6>
-                : <div className="card shadow card-evaluation">
-                    <form onSubmit={handleOnSubmit}>
-                        <div className="filter-block">
-                            <p className="label-filter">Lựa chọn năm</p>
-                            <div className="form-filter">
-                                <div className="year-input">
-                                    <Select 
-                                        placeholder="Lựa chọn năm" 
-                                        isClearable={true} 
-                                        value={null} 
-                                        options={yearForFilter} 
-                                        onChange={handleChangeSelectInput} />
-                                </div>
-                                <button type="submit" className="btn-filter"><Image src={IconLoop} alt='Loop' />Tìm kiếm</button>
+            <div className="card shadow card-evaluation">
+                <form onSubmit={handleOnSubmit}>
+                    <div className="filter-block">
+                        <p className="label-filter">Lựa chọn năm</p>
+                        <div className="form-filter">
+                            <div className="year-input">
+                                <Select 
+                                    placeholder="Lựa chọn năm" 
+                                    isClearable={true} 
+                                    value={year} 
+                                    options={years} 
+                                    onChange={handleChangeSelectInput} />
                             </div>
+                            <button type="submit" className="btn-filter"><Image src={IconLoop} alt='Loop' />Tìm kiếm</button>
                         </div>
-                    </form>
+                    </div>
+                </form>
+                {
+                    evaluationForms?.length > 0 ?
                     <div className="wrap-table-list-evaluation">
                         <table className='table-list-evaluation'>
                             <thead>
@@ -139,24 +158,13 @@ function MyEvaluation(props) {
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td className='c-form-name'><div className='form-name'>Biểu mẫu đánh giá Vin3S Q4/2021</div></td>
-                                    <td className='c-created-date text-center'><div className='created-date'>01/03/2021</div></td>
-                                    <td className='c-status text-center'><div className='status'>Đang đánh giá</div></td>
-                                    <td className='c-step text-center'><div className='step'>QTTT đánh giá</div></td>
-                                </tr>
-                                <tr>
-                                    <td className='c-form-name'><div className='form-name'>Biểu mẫu đánh giá Vin3S Q3/2021</div></td>
-                                    <td className='c-created-date text-center'><div className='created-date'>01/06/2021</div></td>
-                                    <td className='c-status text-center'><div className='status'>Đã hoàn thành</div></td>
-                                    <td className='c-step text-center'><div className='step'>QTTT đánh giá</div></td>
-                                </tr>
+                                { renderListEvaluationForms() }
                             </tbody>
                         </table>
                     </div>
-                </div>
-            }
-            {/* <CustomPaging pageSize={parseInt(paging?.pageSize)} onChangePage={onChangePage} totalRecords={projectData?.totalRecord} /> */}
+                    : <h6 className="alert alert-danger not-found-data" role="alert">{t("NoDataFound")}</h6>
+                }
+            </div>
         </div>
         </>
     )
