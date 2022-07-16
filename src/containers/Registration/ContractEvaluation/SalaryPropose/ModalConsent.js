c23ed4dc679ee070c14a4f23a773af0a8692e303
HOANG VAN QUYET <v.quyethv1@vinfast.vn>
2022-07-16 09:03:20 +0700

    Code folow de xuat tang luong

--- src/containers/Registration/ContractEvaluation/SalaryPropose/ModalConsent.js
+++ src/containers/Registration/ContractEvaluation/SalaryPropose/ModalConsent.js
@@ -1,16 +1,18 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Modal } from 'react-bootstrap';
 import IconDelete from '../../../../assets/img/icon/Icon_Cancel.svg';
 import IconX from '../../../../assets/img/icon/icon_x.svg';
 import { useTranslation } from 'react-i18next';
 import './styles.scss';
-const ModalConsent = ({ show, onHide, data, setData, onConfirm }) => {
+
+const ModalConsent = ({ show, type, header, title, onHide, data, setData, onConfirm }) => {
   const { t } = useTranslation();
   const [error, setError] = useState(false)
+
   return (
     <Modal centered show={show} onHide={onHide} className='modal-consent'>
       <Modal.Header className='modal-consent__header'>
-        <p className='modal-consent__title'>{t('confirmCancleConsent')}</p>
+        <p className='modal-consent__title'>{header}</p>
         <img
           src={IconX}
           className='mr-1 cursor-pointer icon-delete'
@@ -21,12 +23,12 @@ const ModalConsent = ({ show, onHide, data, setData, onConfirm }) => {
         <div className='content'>
           <div>
             <p className='modal-consent__textlabel'>
-              Lý do từ chối thẩm định <span className='text-danger'>(*)</span>
+              {title} <span className='text-danger'>(*)</span>
             </p>
             <textarea
               className='modal-consent__textarea'
               rows={3}
-              placeholder=' Không điều chỉnh lương'
+              placeholder='Nhập lý do'
               value={data}
               onChange={(val) => {
                 if (val.target.value !== '') {
@@ -35,7 +37,7 @@ const ModalConsent = ({ show, onHide, data, setData, onConfirm }) => {
                 setData(val.target.value);
               }}
             ></textarea>
-            {error &&<span className='text-danger text-xs'>{t('RequiredInput')}</span>}
+            {error && <span className='text-danger text-xs'>{t('RequiredInput')}</span>}
           </div>
           <div className='d-flex justify-content-end mt-3'>
             <button
@@ -48,7 +50,7 @@ const ModalConsent = ({ show, onHide, data, setData, onConfirm }) => {
             </button>
             <button
               type='button'
-              className='btn btn-danger ml-3 shadow'
+              className={type === 'approve' ? 'btn btn-success ml-3 shadow' : 'btn btn-danger ml-3 shadow'}
               onClick={() => {
                 if (data.trim().length === 0) {
                   setError(true);
