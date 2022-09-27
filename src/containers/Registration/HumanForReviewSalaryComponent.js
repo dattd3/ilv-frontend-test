9e1ac8aa115941f96f5ab90a6d4162b1374f600f
VINGROUP\quyethv1 <v.quyethv1@vingroup.net>
2022-09-27 16:57:29 +0700

    Update key position_name

--- src/containers/Registration/HumanForReviewSalaryComponent.js
+++ src/containers/Registration/HumanForReviewSalaryComponent.js
@@ -205,7 +205,7 @@ class HumanForReviewSalaryComponent extends React.Component {
                 pnl: res.pnl,
                 orglv2Id: res.organization_lv2,
                 account: res.username,
-                current_position: res.postition_name,
+                current_position: res.position_name,
                 department: res.division + (res.department ? '/' + res.department : '') + (res.part ? '/' + res.part : '')
               }
             })
