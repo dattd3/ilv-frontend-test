270e9df78364577ca2e37fc8b2bde9010fc02e12
Nguyen Manh Hieu <hieu>
2023-01-07 17:11:14 +0700

    feat: add api get target list

--- src/containers/Evaluation/TargetManagement/index.js
+++ src/containers/Evaluation/TargetManagement/index.js
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Select from "react-select";
 import { useTranslation } from "react-i18next";
 import { Button } from "react-bootstrap";
+import ReactTooltip from "react-tooltip";
+import axios from "axios";
 import { ReactComponent as IconFilter } from "assets/img/ic-filter.svg";
+import { ReactComponent as IconReason } from "assets/img/ic-reason.svg";
 import HOCComponent from "components/Common/HOCComponent";
 import CustomPaging from "components/Common/CustomPagingNew";
+import { getRequestConfigurations } from "commons/Utils";
+import LoadingModal from "components/Common/LoadingModal";
 
 const filterPlaceholder = (text) => (
   <div>
@@ -15,16 +20,119 @@ const filterPlaceholder = (text) => (
   </div>
 );
 
+const getStatusTagStyle = (value) => {
+  switch (value) {
+    case 3:
+      return {
+        color: "#05BD29",
+        border: "1px solid #05BD2",
+      };
+    case 4:
+      return {
+        color: "#FF0000",
+        border: "1px solid #FF0000",
+      };
+
+    default:
+      return {
+        color: "#000",
+        border: "1px solid #DEE2E6",
+      };
+  }
+};
+
+const CHECK_PHASE_LIST_ENDPOINT = `${process.env.REACT_APP_HRDX_PMS_URL}api/checkphase/list`;
+const FETCH_TARGET_LIST_ENDPOINT = `${process.env.REACT_APP_HRDX_PMS_URL}api/targetregist/list`;
+
 function TargetManagement() {
+  const [phaseOptions, setPhaseOptions] = useState([]);
+  const [targetRegistration, setTargetRegistration] = useState([]);
+  const [isShowLoadingModal, setIsShowLoadingModal] = useState([]);
   const [pageSize, setPageSize] = useState(5);
   const [pageIndex, setPageIndex] = useState(1);
+  const [phaseIdSelected, setPhaseIdSelected] = useState(0);
+  const [statusSelected, setStatusSelected] = useState(0);
+
   const [totalRecords, setTotalRecords] = useState(0);
   const [openMenuRegistration, setOpenMenuRegistration] = useState(false);
-
   const { t } = useTranslation();
+  const config = getRequestConfigurations();
+
+  useEffect(() => {
+    fetchInitData();
+  }, []);
+
+  useEffect(() => {
+    if (pageSize && pageIndex) {
+      fetchTargetList();
+    }
+  }, [pageSize, pageIndex]);
+
+  const fetchInitData = () => {
+    const bodyFormData = new FormData();
+    bodyFormData.append("nopaging", true);
+    axios
+      .post(CHECK_PHASE_LIST_ENDPOINT, bodyFormData, config)
+      .then((res) => {
+        if (res?.data?.data) {
+          setPhaseOptions(
+            res.data.data.map((item) => ({
+              value: item.id,
+              label: item.name,
+            }))
+          );
+        }
+      })
+      .catch((error) => {});
+  };
+
+  const fetchTargetList = async () => {
+    setIsShowLoadingModal(true);
+    try {
+      const response = await axios.post(
+        FETCH_TARGET_LIST_ENDPOINT,
+        {
+          pageIndex,
+          pageSize,
+          checkPhaseId: phaseIdSelected,
+          status: statusSelected,
+          EmployeeCode: localStorage.getItem("employeeNo"),
+          type: "OWNER",
+        },
+        config
+      );
+      if (response?.data?.data) {
+        setTargetRegistration(response.data.data);
+      }
+      if (response?.data?.result) {
+        setTotalRecords(response.data.result.totalRecords);
+      }
+    } catch (error) {}
+    setIsShowLoadingModal(false);
+  };
+
+  const STATUS_OPTIONS = [
+    {
+      label: t("Draft"),
+      value: 1,
+    },
+    {
+      label: t("Processing"),
+      value: 2,
+    },
+    {
+      label: t("Approved"),
+      value: 3,
+    },
+    {
+      label: t("Reject"),
+      value: 4,
+    },
+  ];
 
   return (
     <div className="target-management-page">
+      <LoadingModal show={isShowLoadingModal} />
       <div className="menu-btns">
         <Button className="button primary-button">{t("Request")}</Button>
         <Button className="button">{t("Menu_Task_Approval")}</Button>
@@ -41,11 +149,13 @@ function TargetManagement() {
             + {t("TargetRegistration")}
           </Button>
           {openMenuRegistration && (
-            <div
-              className="menu-registration"
-            >
-              <div className="menu-registration-option">{t('TargetRegistrationManual')}</div>
-              <div className="menu-registration-option">{t('TargetRegistrationLibrary')}</div>
+            <div className="menu-registration">
+              <div className="menu-registration-option">
+                {t("TargetRegistrationManual")}
+              </div>
+              <div className="menu-registration-option">
+                {t("TargetRegistrationLibrary")}
+              </div>
             </div>
           )}
         </div>
@@ -53,13 +163,23 @@ function TargetManagement() {
           className="select-container"
           classNamePrefix="filter-select"
           placeholder={filterPlaceholder(t("AssessmentPeriod"))}
+          options={phaseOptions}
+          onChange={(val) => setPhaseIdSelected(val?.value || 0)}
+          isClearable
         />
         <Select
           className="select-container"
           classNamePrefix="filter-select"
           placeholder={filterPlaceholder(t("Status"))}
+          options={STATUS_OPTIONS}
+          onChange={(val) => setStatusSelected(val?.value || 0)}
+          isClearable
         />
-        <Button className="button search-button" variant="warning">
+        <Button
+          className="button search-button"
+          variant="warning"
+          onClick={fetchTargetList}
+        >
           {t("Search")}
         </Button>
       </div>
@@ -71,33 +191,58 @@ function TargetManagement() {
               <th>{t("RequestNo")}</th>
               <th>{t("RegistrationType")}</th>
               <th>{t("AssessmentPeriod")}</th>
+              <th className="text-center">{t("TotalTarget")}</th>
               <th>{t("Requestor")}</th>
               <th>{t("ADCode")}</th>
-              <th>{t("Status")}</th>
-              <th>{t("Reason")}</th>
-              <th>{t("action")}</th>
+              <th className="text-center">{t("Status")}</th>
+              <th className="text-center">{t("Reason")}</th>
+              <th className="text-center">{t("action")}</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-              <td>6</td>
-              <td>7</td>
-            </tr>
+            {targetRegistration.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.requestType}</td>
+                <td>{item.checkPhaseName}</td>
+                <td className="text-center">{item.totalTarget}</td>
+                <td>{JSON.parse(item.userInfo)?.fullName}</td>
+                <td>{JSON.parse(item.userInfo)?.account}</td>
+                <td className="text-center">
+                  <div
+                    className="status-tag"
+                    style={getStatusTagStyle(item.status)}
+                  >
+                    {STATUS_OPTIONS.find((i) => i.value === item.status)?.label}
+                  </div>
+                </td>
+                <td className="text-center">
+                  <a
+                    data-for={`${item.reason}-${item.id}`}
+                  >
+                    <IconReason />
+                  </a>
+                  <ReactTooltip
+                    id={`${item.reason}-${item.id}`}
+                    scrollHide
+                    isCapture
+                    clickable
+                    place="left"
+                    backgroundColor="#FFFFFF"
+                    arrowColor="#FFFFFF"
+                  >
+                    <div>ssss</div>
+                  </ReactTooltip>
+                </td>
+                <td className="text-center"></td>
+              </tr>
+            ))}
           </tbody>
         </table>
         <div className="table-footer">
           <CustomPaging
-            onChangePageSize={(page) => {
-              setPageSize(page);
-            }}
-            onChangePageIndex={(size) => {
-              setPageIndex(size);
-            }}
+            onChangePageSize={setPageSize}
+            onChangePageIndex={setPageIndex}
             totalRecords={totalRecords}
             pageSize={pageSize}
             pageIndex={pageIndex}
