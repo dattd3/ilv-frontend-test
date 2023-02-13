e2b34869f11225ee033dba7e001f74ed54fdb6bd
Nguyen Manh Hieu <hieu>
2023-02-13 09:49:57 +0700

    fix: bug warning modal

--- src/components/Common/ConfirmModalNew.js
+++ src/components/Common/ConfirmModalNew.js
@@ -20,7 +20,7 @@ export default function ConfirmModal(props) {
     <Modal
       centered
       show={show}
-      className={clsx("zi-1040 confirm-modal-new", modalClassName)}
+      className={clsx("zi-1040 confirm-modal-new2", modalClassName)}
       onHide={onHide}
     >
       <Modal.Header closeButton>
