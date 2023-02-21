107408bba9c73f93d97e7d04d83021dabce785a5
VINGROUP\datth3 <v.datth3@vin3s.vn>
2023-02-21 15:58:51 +0700

    [ILVG-588][resign] Update phiếu phỏng vấn nghỉ việc

--- src/containers/Registration/TextareaComponent.js
+++ src/containers/Registration/TextareaComponent.js
@@ -1,4 +1,112 @@
 import React from 'react';
+import { useRef } from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+const ResizableNewTextarea = (props) => {
+	const [state, setState] = useState({
+		value: '',
+		rows: 1,
+		minRows: 1,
+		maxRows: 10,
+		firstTime: true,
+		handleChange: false
+	});
+	const selectRef = useRef()
+
+	useEffect(() => {
+		if(selectRef.current && state.firstTime) {
+			const textareaLineHeight = 24;
+            const { minRows, maxRows, value } = state;
+
+            let currentRows = ~~( selectRef.current.scrollHeight / textareaLineHeight);
+			if(props.minRows > 1 && currentRows < props.minRows ) {
+				currentRows = props.minRows || minRows;
+			}
+			if(currentRows > (props.maxRows || state.maxRows)) {
+				currentRows = props.maxRows || state.maxRows;
+			}
+            if(currentRows == state.rows){
+				setState({
+					...state,
+					firstTime: false
+				}); 
+                return;
+            }
+			
+            setState({
+				...state,
+                rows: currentRows,
+				firstTime: false
+            }); 
+		}
+
+		if(selectRef.current && props.value && props.value != state.value && state.handleChange == false) {
+			const textareaLineHeight = 24;
+            const { minRows, maxRows, value } = state;
+
+            let currentRows = ~~( selectRef.current.scrollHeight / textareaLineHeight);
+			if(props.minRows > 1 && currentRows < props.minRows ) {
+				currentRows = props.minRows || minRows;
+			}
+			if(currentRows > (props.maxRows || state.maxRows)) {
+				currentRows = props.maxRows || state.maxRows;
+			}
+			setState({
+				...state,
+				value: props.value,
+				rows: currentRows,
+			})
+		}
+	}, [props.value, state.firstTime, selectRef, state.handleChange]);
+
+	const handleChange = (event) => {
+		const textareaLineHeight = 24;
+		const {value} = state;
+		const minRows = props.minRows || state.minRows;
+		const maxRows = props.maxRows || state.maxRows
+		const previousRows = event.target.rows;
+  	    event.target.rows = minRows; // reset number of rows in textarea 
+		
+		const currentRows = ~~(event.target.scrollHeight / textareaLineHeight);
+		if(minRows > 1 && currentRows < minRows) {
+			currentRows = minRows;
+		}
+        if (currentRows === previousRows) {
+            event.target.rows = currentRows;
+        }
+		
+		if (currentRows >= maxRows) {
+			event.target.rows = maxRows;
+			event.target.scrollTop = event.target.scrollHeight;
+		}
+		
+    
+        setState({
+			...state,
+            value: event.target.value,
+            rows: currentRows < maxRows ? currentRows : maxRows,
+            firstTime: false,
+			handleChange: true
+        });
+        if(props.onChange){
+            props.onChange(event);
+        }
+	};
+
+	return (
+		<textarea
+			rows={state.rows}
+			value={props.value}
+			placeholder={props.placeholder || ''}
+			className={'textarea ' + (props.className || '')}
+			onChange={handleChange}
+			ref={selectRef}
+			disabled={props.disabled || false}
+		/>
+	);
+
+}
 
 class ResizableTextarea extends React.Component {
 	constructor(props) {
@@ -14,13 +122,16 @@ class ResizableTextarea extends React.Component {
 	
 	handleChange = (event) => {
 		const textareaLineHeight = 24;
-		const { minRows, maxRows } = this.state;
-		
+		const {value} = this.state;
+		const minRows = this.props.minRows || this.state.minRows;
+		const maxRows = this.props.maxRows || this.state.maxRows
 		const previousRows = event.target.rows;
   	    event.target.rows = minRows; // reset number of rows in textarea 
 		
 		const currentRows = ~~(event.target.scrollHeight / textareaLineHeight);
-    
+		if(minRows > 1 && currentRows < minRows) {
+			currentRows = minRows;
+		}
         if (currentRows === previousRows) {
             event.target.rows = currentRows;
         }
@@ -29,6 +140,7 @@ class ResizableTextarea extends React.Component {
 			event.target.rows = maxRows;
 			event.target.scrollTop = event.target.scrollHeight;
 		}
+		
     
         this.setState({
             value: event.target.value,
@@ -47,12 +159,20 @@ class ResizableTextarea extends React.Component {
 	}
 
     processFirstTime(e){
+		console.log('begin>>>>', this.props.value, this.state.firstTime);
         if(e && this.state.firstTime){
             const textareaLineHeight = 24;
-            const { minRows, maxRows } = this.state;
-            const currentRows = ~~( e.scrollHeight / textareaLineHeight);
-			
+            const { minRows, maxRows, value } = this.state;
+
+            let currentRows = ~~( e.scrollHeight / textareaLineHeight);
+			console.log('processFirstTime>>>', this.props.value, currentRows, this.props.minRows);
+			if(this.props.minRows > 1 && currentRows < this.props.minRows ) {
+				currentRows = this.props.minRows || minRows;
+			}
             if(currentRows == this.state.rows){
+				this.setState({
+					firstTime: false
+				}); 
                 return;
             }
 			
@@ -62,12 +182,12 @@ class ResizableTextarea extends React.Component {
             }); 
         }
     }
-	
+
 	render() {
 		return (
 			<textarea
 				rows={this.state.rows}
-				value={this.props.value || this.state.value}
+				value={this.props.value}
 				placeholder={this.props.placeholder || ''}
 				className={'textarea ' + (this.props.className || '')}
 				onChange={this.handleChange}
@@ -77,4 +197,4 @@ class ResizableTextarea extends React.Component {
 		);
 	}
 }
-export default ResizableTextarea;
+export default ResizableNewTextarea;
