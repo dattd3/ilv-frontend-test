6795cd50e71a0a8966f29db1b326668bc36e5f8c
VINGROUP\datth3 <v.datth3@vin3s.vn>
2023-02-25 13:50:05 +0700

    [Config] Mở phân hệ nghỉ việc cho Vinhome PRODUCTION

--- src/commons/Constants.js
+++ src/commons/Constants.js
@@ -216,7 +216,7 @@ const Constants = {
     1: ["V040", "V005", "V079", "V041", "V030"],//TUYENDUNG + V061
     2: ["V040", "V005", "V061", "V077"],//DANHGIA_TAIKI
     3: [],//DEXUATLUONG
-    4: [],//NGHIVIEC
+    4: ["V040"],//NGHIVIEC
     5: []//PHUCLOI
   } :
   { //Development
