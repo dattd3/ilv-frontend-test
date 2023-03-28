56e26df2d190aaa9a9a470f47d96be72726bf746
Nguyễn Văn Cường <cuongnv56@vingroup.net>
2023-03-28 15:21:15 +0700

    Hot fix employee timesheet show OT line

--- src/commons/Utils.js
+++ src/commons/Utils.js
@@ -37,6 +37,10 @@ const formatStringByMuleValue = value => {
     return (value === null || value === undefined || value === "" || value === "#") ? "" : value.trim()
 }
 
+const formatStringDateTimeByMuleValue = value => {
+    return (value === null || value === undefined || value === "" || value === "#" || value === "000000") ? "" : value.trim()
+}
+
 const formatNumberInteger = value => {
     if (isNaN(value)) {
         return 0
@@ -376,5 +380,5 @@ const getCurrentLanguage = () => {
 export {
     getRequestConfigurations, removeAccents, formatStringByMuleValue, formatNumberInteger, exportToPDF, isEnableFunctionByFunctionName, getValueParamByQueryString, getDateByRangeAndFormat,
     calculateBackDateByPnLVCodeAndFormatType, isEnableShiftChangeFunctionByPnLVCode, isEnableInOutTimeUpdateFunctionByPnLVCode, getRequestTypeIdsAllowedToReApproval, getMuleSoftHeaderConfigurations,
-    isAdjacentDateBy2Date, showRangeDateGroupByArrayDate, generateTaskCodeByCode, parsteStringToHtml, getRegistrationMinDateByConditions, isVinFast, getCurrentLanguage
+    isAdjacentDateBy2Date, showRangeDateGroupByArrayDate, generateTaskCodeByCode, parsteStringToHtml, getRegistrationMinDateByConditions, isVinFast, getCurrentLanguage, formatStringDateTimeByMuleValue
 }
