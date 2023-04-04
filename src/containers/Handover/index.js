7afc0ac33b7c09ffcaa9ef36ae45a2a2d463daa7
VINGROUP\datth3 <v.datth3@vin3s.vn>
2023-04-04 12:20:42 +0700

    [ILVG-807] CR VH cho phép QLTT người nhận bàn giao sửa thông tin

--- src/containers/Handover/index.js
+++ src/containers/Handover/index.js
@@ -35,7 +35,7 @@ class LeaveOfAbsenceDetailComponent extends React.Component {
     const currentEmployeeNo = localStorage.getItem('email');
     const canEditable = {...this.state.canEditable};
     const data = this.state.data;
-    
+    let employeeEmail = this.state.data.employee.employeeEmail;
     for (const [key, value] of Object.entries(this.state.data)) {
       if(key === 'employee') {
         let filled = null;
@@ -51,14 +51,17 @@ class LeaveOfAbsenceDetailComponent extends React.Component {
         let filled = data[key] && data[key].status ? null : '(Bắt buộc)';
         if(value && value.user && value.user.account && (value.user.account.toLowerCase()  + '@vingroup.net') == currentEmployeeNo.toLowerCase() ) {
           canEditable[key] = true;
+          canEditable['hanover_' + key] = !data[key].status;
           canEditable['canUpdate'] = true;
           canEditable['employee'] = false;
           if(!canEditable.currentActive.includes("employee"))
             canEditable.currentActive.push(key);
         }
+        if(this.state.isViewAll && data[key].status != true && employeeEmail.toLowerCase()  != currentEmployeeNo.toLowerCase()) {
+          canEditable['manager_' + key] = true;
+        }
       }
     }
-
     this.setState({
       canEditable: canEditable
     })
@@ -185,7 +188,7 @@ class LeaveOfAbsenceDetailComponent extends React.Component {
         const result = res.data.result;
         if (result.code != Constants.API_ERROR_CODE) {
           const responseData = this.saveStateInfos(res.data.data);
-          this.setState({data : responseData.data, remoteData: responseData.remoteData }, () => {
+          this.setState({data : responseData.data, remoteData: responseData.remoteData, isViewAll: res.data.data.isViewAll }, () => {
             this.checkAuthorize();
           });
         }
@@ -195,8 +198,8 @@ class LeaveOfAbsenceDetailComponent extends React.Component {
   }
 
   prepareDataToSubmit = (data, remoteData) => {
-    const isEmployee = this.state.canEditable.currentActive.includes("employee");
-    if(isEmployee) {
+    //const isEmployee = this.state.canEditable.currentActive.includes("employee");
+    if(this.state.isViewAll) {
       remoteData.contractTerminationInfoId  = this.props.match.params.id;
       remoteData.projessionlWork = data.job.mainWork;
       remoteData.controlWord = data.job.controlWork;
@@ -217,58 +220,69 @@ class LeaveOfAbsenceDetailComponent extends React.Component {
         switch(key) {
           case 'job':
             remoteData.dateHandoverWork = data.job.actionDate;
-            remoteData.statusWork = data.job.status;
+            remoteData.statusWork = data.job.status ? 1 : 0;
             remoteData.noteWord = data.job.note;
+            remoteData.handoverWork = data.job.user;
             break;
           case 'asset':
             remoteData.dateImplementationAsset = data.asset.actionDate;
-            remoteData.statusAsset = data.asset.status;
+            remoteData.statusAsset = data.asset.status ? 1 : 0;
             remoteData.noteAsset = data.asset.note;
+            remoteData.handoverAsset = data.asset.user;
             break;
           case 'taxi':
             remoteData.vehicleCardDate = data.taxi.actionDate;
-            remoteData.vehicleCardStatus = data.taxi.status;
+            remoteData.vehicleCardStatus = data.taxi.status ? 1 : 0;
             remoteData.vehicleCardNote = data.taxi.note;
+            remoteData.vehicleCard = data.taxi.user?.account ? data.taxi.user : null;
             break;
           case 'associateCard':
             remoteData.dateHandoverSocial = data.associateCard.actionDate;
-            remoteData.statusSocial = data.associateCard.status;
+            remoteData.statusSocial = data.associateCard.status ? 1 : 0;
             remoteData.noteSocial = data.associateCard.note;
+            remoteData.handoverSocial = data.associateCard.user;
             break;
           case 'uniform':
             remoteData.dateHandoverUniform = data.uniform.actionDate;
-            remoteData.statusUniform = data.uniform.status;
+            remoteData.statusUniform = data.uniform.status ? 1 : 0;
             remoteData.noteUniform = data.uniform.note;
+            remoteData.handoverUniform = data.uniform.user;
             break;
           case 'finger':
             remoteData.dateHandoverFingerEmail = data.finger.actionDate;
-            remoteData.statusFingerprintEmail = data.finger.status;
+            remoteData.statusFingerprintEmail = data.finger.status ? 1 : 0;
             remoteData.noteFingerprintEmail = data.finger.note;
+            remoteData.handoverFingerprintEmail = data.finger.user;
             break;
           case 'inout':
             remoteData.dateHandoverDebt = data.inout.actionDate;
-            remoteData.statusDebt = data.inout.status;
+            remoteData.statusDebt = data.inout.status ? 1 : 0;
             remoteData.noteDebt = data.inout.note;
+            remoteData.handoverDebt = data.inout.user;
             break;
           case 'tool':
             remoteData.dateHandoverSoftware = data.tool.actionDate;
-            remoteData.statusSoftware = data.tool.status;
+            remoteData.statusSoftware = data.tool.status ? 1 : 0;
             remoteData.noteSoftware = data.tool.note;
+            remoteData.handoverSoftware = data.tool.user?.account ? data.tool.user : null ;
             break;
           case 'policy':
             remoteData.dateHandoverConfirmation = data.policy.actionDate;
-            remoteData.statusConfirmation = data.policy.status;
+            remoteData.statusConfirmation = data.policy.status ? 1 : 0;
             remoteData.noteConfirmation = data.policy.note;
+            remoteData.handoverConfirmation = data.policy.user?.account ? data.policy.user : null;
             break;
           case 'traning':
             remoteData.trainingDebtDate = data.traning.actionDate;
-            remoteData.trainingDebtStatus = data.traning.status;
+            remoteData.trainingDebtStatus = data.traning.status ? 1 : 0;
             remoteData.trainingDebtNote = data.traning.note;
+            remoteData.trainingDebt = data.traning.user?.account ? data.traning.user : null;
             break;
           case 'internal':
             remoteData.internalDebtDate = data.internal.actionDate;
-            remoteData.internalDebtStatus = data.internal.status;
+            remoteData.internalDebtStatus = data.internal.status ? 1 : 0;
             remoteData.internalDebtNote = data.internal.note;
+            remoteData.internalDebt = data.internal.user?.account ? data.internal.user : null;
             break;
 
         }
@@ -367,7 +381,7 @@ class LeaveOfAbsenceDetailComponent extends React.Component {
   verifyInputs = () => {
     const errors = {};
     const data = this.state.data;
-    const isEmployee = this.state.canEditable.currentActive.includes("employee");
+    const isEmployee = true//this.state.canEditable.currentActive.includes("employee");
     let noRequireFields = ['employee', 'tool', 'policy', 'traning', 'internal'];
     if(!checkIsExactPnL(Constants.pnlVCode.VinFast, Constants.pnlVCode.VinFastTrading)) {
       noRequireFields.push('taxi');
@@ -417,11 +431,23 @@ class LeaveOfAbsenceDetailComponent extends React.Component {
   }
 
   updateApprover(name, approver, isApprover) {
+    const _canEditable = {...this.state.canEditable};
+    if(_canEditable['hanover_' + name] == true) {
+      if(!approver || approver.account.toLowerCase()  + '@vingroup.net' != localStorage.getItem('email').toLowerCase()) {
+        _canEditable[name] = false;
+      } else {
+        _canEditable[name] = true;
+      }
+    }
     this.setState({
+      canEditable: _canEditable,
       data: {
         ...this.state.data,
         [name]: {
           ...this.state.data[name],
+          actionDate: '',
+          status: false,
+          note: '',
           user: {
             ...approver,
             avatar: null
@@ -560,7 +586,7 @@ class LeaveOfAbsenceDetailComponent extends React.Component {
           <div className="row">
             <div className="col-4">
                 {t('user_action')}
-                <ApproverComponent isEdit={!disableComponent.employee} approver={data.job.user}  updateApprover={(approver, isApprover) => this.updateApprover('job', approver,isApprover )} errors={{approver: this.state.errors['job']}}/>
+                <ApproverComponent isEdit={!(disableComponent.employee || disableComponent.manager_job || disableComponent.hanover_job)} approver={data.job.user}  updateApprover={(approver, isApprover) => this.updateApprover('job', approver,isApprover )} errors={{approver: this.state.errors['job']}}/>
             </div>
             <div className="col-4">
                 {t('handover_date')}
@@ -593,7 +619,7 @@ class LeaveOfAbsenceDetailComponent extends React.Component {
                 <div className="row">
                     <div className="col-4">
                         {t('user_action')}
-                        <ApproverComponent isEdit={!disableComponent.employee} approver={data.asset.user}  updateApprover={(approver, isApprover) => this.updateApprover('asset', approver,isApprover )} errors={{approver: this.state.errors['asset']}}/>
+                        <ApproverComponent isEdit={!(disableComponent.employee || disableComponent.manager_asset || disableComponent.hanover_asset)} approver={data.asset.user}  updateApprover={(approver, isApprover) => this.updateApprover('asset', approver,isApprover )} errors={{approver: this.state.errors['asset']}}/>
                     </div>
                     <div className="col-4">
                         {t('handover_date')}
@@ -618,7 +644,7 @@ class LeaveOfAbsenceDetailComponent extends React.Component {
                 <div className="row">
                     <div className="col-4">
                         {t('user_action')}
-                        <ApproverComponent isEdit={!disableComponent.employee} approver={data.taxi.user}  updateApprover={(approver, isApprover) => this.updateApprover('taxi', approver,isApprover )} errors={{approver: this.state.errors['taxi']}}/>
+                        <ApproverComponent isEdit={!(disableComponent.employee || disableComponent.manager_taxi || disableComponent.hanover_taxi)} approver={data.taxi.user}  updateApprover={(approver, isApprover) => this.updateApprover('taxi', approver,isApprover )} errors={{approver: this.state.errors['taxi']}}/>
                     </div>
                     <div className="col-4">
                         {t('handover_date')}
@@ -646,7 +672,7 @@ class LeaveOfAbsenceDetailComponent extends React.Component {
                 <div className="row">
                     <div className="col-4">
                         {t('user_action')}
-                        <ApproverComponent isEdit={!disableComponent.employee} approver={data.asset.user}  updateApprover={(approver, isApprover) => this.updateApprover('asset', approver,isApprover )} errors={{approver: this.state.errors['asset']}}/>
+                        <ApproverComponent isEdit={!(disableComponent.employee|| disableComponent.manager_asset || disableComponent.hanover_asset)} approver={data.asset.user}  updateApprover={(approver, isApprover) => this.updateApprover('asset', approver,isApprover )} errors={{approver: this.state.errors['asset']}}/>
                     </div>
                     <div className="col-4">
                         {t('handover_date')}
@@ -677,7 +703,7 @@ class LeaveOfAbsenceDetailComponent extends React.Component {
             <div className="row">
                 <div className="col-4">
                 {t('user_action')}
-                    <ApproverComponent isEdit={!disableComponent.employee} approver={data.associateCard.user}  updateApprover={(approver, isApprover) => this.updateApprover('associateCard', approver,isApprover )} errors={{approver: this.state.errors['associateCard']}}/>
+                    <ApproverComponent isEdit={!(disableComponent.employee|| disableComponent.manager_associateCard || disableComponent.hanover_associateCard)} approver={data.associateCard.user}  updateApprover={(approver, isApprover) => this.updateApprover('associateCard', approver,isApprover )} errors={{approver: this.state.errors['associateCard']}}/>
                 </div>
                 <div className="col-4">
                 {t('handover_date')}
@@ -703,7 +729,7 @@ class LeaveOfAbsenceDetailComponent extends React.Component {
             <div className="row">
                 <div className="col-4">
                 {t('user_action')}
-                    <ApproverComponent isEdit={!disableComponent.employee} approver={data.uniform.user}  updateApprover={(approver, isApprover) => this.updateApprover('uniform', approver,isApprover )} errors={{approver: this.state.errors['uniform']}}/>
+                    <ApproverComponent isEdit={!(disableComponent.employee|| disableComponent.manager_uniform || disableComponent.hanover_uniform)} approver={data.uniform.user}  updateApprover={(approver, isApprover) => this.updateApprover('uniform', approver,isApprover )} errors={{approver: this.state.errors['uniform']}}/>
                 </div>
                 <div className="col-4">
                 {t('handover_date')}
@@ -729,7 +755,7 @@ class LeaveOfAbsenceDetailComponent extends React.Component {
             <div className="row">
                 <div className="col-4">
                 {t('user_action')}
-                    <ApproverComponent isEdit={!disableComponent.employee} approver={data.finger.user}  updateApprover={(approver, isApprover) => this.updateApprover('finger', approver,isApprover )} errors={{approver: this.state.errors['finger']}}/>
+                    <ApproverComponent isEdit={!(disableComponent.employee || disableComponent.manager_finger || disableComponent.hanover_finger)} approver={data.finger.user}  updateApprover={(approver, isApprover) => this.updateApprover('finger', approver,isApprover )} errors={{approver: this.state.errors['finger']}}/>
                 </div>
                 <div className="col-4">
                 {t('handover_date')}
@@ -755,7 +781,7 @@ class LeaveOfAbsenceDetailComponent extends React.Component {
             <div className="row">
                 <div className="col-4">
                 {t('user_action')}
-                    <ApproverComponent isEdit={!disableComponent.employee } approver={data.inout.user}  updateApprover={(approver, isApprover) => this.updateApprover('inout', approver,isApprover )} errors={{approver: this.state.errors['inout']}} />
+                    <ApproverComponent isEdit={!(disableComponent.employee || disableComponent.manager_inout || disableComponent.hanover_inout) } approver={data.inout.user}  updateApprover={(approver, isApprover) => this.updateApprover('inout', approver,isApprover )} errors={{approver: this.state.errors['inout']}} />
                 </div>
                 <div className="col-4">
                 {t('handover_date')}
@@ -781,7 +807,7 @@ class LeaveOfAbsenceDetailComponent extends React.Component {
             <div className="row">
                 <div className="col-4">
                 {t('user_action')}
-                    <ApproverComponent isEdit={!disableComponent.employee} approver={data.tool.user}  updateApprover={(approver, isApprover) => this.updateApprover('tool', approver,isApprover )} errors={{approver: this.state.errors['tool']}}/>
+                    <ApproverComponent isEdit={!(disableComponent.employee|| disableComponent.manager_tool || disableComponent.hanover_tool)} approver={data.tool.user}  updateApprover={(approver, isApprover) => this.updateApprover('tool', approver,isApprover )} errors={{approver: this.state.errors['tool']}}/>
                 </div>
                 <div className="col-4">
                 {t('handover_date')}
@@ -807,7 +833,7 @@ class LeaveOfAbsenceDetailComponent extends React.Component {
             <div className="row">
                 <div className="col-4">
                 {t('user_action')}
-                    <ApproverComponent isEdit={!disableComponent.employee} approver={data.policy.user}  updateApprover={(approver, isApprover) => this.updateApprover('policy', approver,isApprover )} errors={{approver: this.state.errors['policy']}}/>
+                    <ApproverComponent isEdit={!(disableComponent.employee|| disableComponent.manager_policy || disableComponent.hanover_policy)} approver={data.policy.user}  updateApprover={(approver, isApprover) => this.updateApprover('policy', approver,isApprover )} errors={{approver: this.state.errors['policy']}}/>
                 </div>
                 <div className="col-4">
                 {t('handover_date')}
@@ -836,7 +862,7 @@ class LeaveOfAbsenceDetailComponent extends React.Component {
                 <div className="row">
                     <div className="col-4">
                     {t('user_action')}
-                        <ApproverComponent isEdit={!disableComponent.employee} approver={data.traning.user}  updateApprover={(approver, isApprover) => this.updateApprover('traning', approver,isApprover )} errors={{approver: this.state.errors['traning']}}/>
+                        <ApproverComponent isEdit={!(disableComponent.employee|| disableComponent.manager_traning || disableComponent.hanover_traning)} approver={data.traning.user}  updateApprover={(approver, isApprover) => this.updateApprover('traning', approver,isApprover )} errors={{approver: this.state.errors['traning']}}/>
                     </div>
                     <div className="col-4">
                     {t('handover_date')}
@@ -862,7 +888,7 @@ class LeaveOfAbsenceDetailComponent extends React.Component {
                 <div className="row">
                     <div className="col-4">
                     {t('user_action')}
-                        <ApproverComponent isEdit={!disableComponent.employee} approver={data.internal.user}  updateApprover={(approver, isApprover) => this.updateApprover('internal', approver,isApprover )} errors={{approver: this.state.errors['internal']}}/>
+                        <ApproverComponent isEdit={!(disableComponent.employee|| disableComponent.manager_internal || disableComponent.hanover_internal)} approver={data.internal.user}  updateApprover={(approver, isApprover) => this.updateApprover('internal', approver,isApprover )} errors={{approver: this.state.errors['internal']}}/>
                     </div>
                     <div className="col-4">
                     {t('handover_date')}
