d2e58fc78188267722e8a4791ce8d226d4da8c8d
Nguyen Manh Hieu <hieu>
2023-04-12 15:02:38 +0700

    feat: done timekeeping history & notification

--- src/commons/Firebase.js
+++ src/commons/Firebase.js
@@ -1,17 +1,16 @@
-import axios from 'axios';
-import { initializeApp } from 'firebase/app';
-import { getMessaging, getToken, onMessage } from 'firebase/messaging';
-import { toast } from 'react-toastify';
-import { getRequestConfigurations } from './Utils';
+import axios from "axios";
+import { initializeApp } from "firebase/app";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { getRequestConfigurations } from "./Utils";
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyBMM56lyG-eU4gORGxcGzLUwPlvWUM4cxY',
-    authDomain: 'myvinpearl-693c0.firebaseapp.com',
-    databaseURL: 'https://myvinpearl-693c0.firebaseio.com',
-    projectId: 'myvinpearl-693c0',
-    storageBucket: 'myvinpearl-693c0.appspot.com',
-    messagingSenderId: '325470433070',
-    appId: '1:325470433070:web:25ac00e9e10da38fc8d4bf',
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
   },
   firebaseApp = initializeApp(firebaseConfig),
   messaging = getMessaging(firebaseApp);
@@ -19,45 +18,43 @@ const firebaseConfig = {
 export const FirebaseUpdateToken = async () => {
   try {
     const permission = await Notification.requestPermission();
-    if (permission === 'granted') {
+    if (
+      (!localStorage.getItem("firebaseToken") ||
+        localStorage.getItem("userFirebaseToken") !==
+          localStorage.getItem("email")) &&
+      permission === "granted"
+    ) {
       const token = await getToken(messaging, {
-        vapidKey:
-          'BFRO-mk2_TtRhn-hlwO_IUxzmL6JOHjkr72lCjOZXpWN5k62t4B7NqJiQoWsty1Iy0W8Q0q-gWq1F9jZRW12zik',
+        vapidKey: process.env.REACT_APP_FIREBASE_VAPID_KEY,
       });
       if (!!token) {
-        console.log('FIREBASE TOKEN: ', token);
-        localStorage.setItem('firebaseToken', token);
+        localStorage.setItem("firebaseToken", token);
+        localStorage.setItem(
+          "userFirebaseToken",
+          localStorage.getItem("email")
+        );
         await axios.post(
           `${process.env.REACT_APP_REQUEST_URL}device/updateToken`,
           {
             deviceToken: token,
-            orgLv3: localStorage.getItem('organizationLv3'),
-            orgLv4: localStorage.getItem('organizationLv4'),
-            orgLv5: localStorage.getItem('organizationLv5'),
-            companyCode: localStorage.getItem('companyCode'),
-            platform: 'Web',
+            orgLv3: localStorage.getItem("organizationLv3"),
+            orgLv4: localStorage.getItem("organizationLv4"),
+            orgLv5: localStorage.getItem("organizationLv5"),
+            companyCode: localStorage.getItem("companyCode"),
+            platform: "Web",
           },
           getRequestConfigurations()
         );
       }
-    } else {
-      toast.info(
-        'Vui lòng thêm quyền đẩy thông báo theo hướng dẫn trong trình duyệt'
-      );
     }
-  } catch (err) {}
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const FirebaseMessageListener = () =>
-  new Promise((resolve) => 
-  onMessage(messaging, (payload) => {
-    console.log('========================================');
-    console.log('payload: ', payload);
-    console.log('========================================');
-    resolve(payload)
-  }
-  )
-  // {
-  //   return 0;
-  // }
+  new Promise((resolve) =>
+    onMessage(messaging, (payload) => {
+      resolve(payload);
+    })
   );
