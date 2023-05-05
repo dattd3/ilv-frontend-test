95472eee3d7abfa563409d50ad7e7b1efdd73507
Nguyễn Văn Cường <cuongnv56@vingroup.net>
2023-05-05 17:50:10 +0700

    [ILVGR-1013] Add time for request processing

--- src/containers/Registration/DepartmentTimeSheetDetail.js
+++ src/containers/Registration/DepartmentTimeSheetDetail.js
@@ -4,6 +4,7 @@ import _ from 'lodash'
 import { withTranslation } from "react-i18next"
 import DetailButtonComponent from './DetailButtonComponent'
 import ApproverDetailComponent from './ApproverDetailComponent'
+import RequestProcessing from './RequestProcessing'
 import StatusModal from '../../components/Common/StatusModal'
 import Constants from '../.../../../commons/Constants'
 import TableUtil from '../../components/Common/table'
@@ -176,6 +177,12 @@ class DepartmentTimeSheetDetail extends React.Component {
               <ApproverDetailComponent title={t("Approver")} approver={this.props.substitution.approver} status={this.props.substitution.processStatusId} hrComment={this.props.substitution.approverComment} />
             </> : null
         }
+
+        <RequestProcessing 
+          createDate={this.props.substitution?.createDate} 
+          deletedDate={this.props.substitution?.deletedDate}
+          assessedDate={this.props.substitution?.assessedDate} 
+          approvedDate={this.props.substitution?.approvedDate} />
 
         {
           this.props.substitution.requestDocuments.length > 0 ?
