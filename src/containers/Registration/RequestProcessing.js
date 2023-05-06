156057df570fb3d5d7636bb1c9b8e8dd43e76e75
Nguyễn Văn Cường <cuongnv56@vingroup.net>
2023-05-06 10:59:57 +0700

    [ILVGR-1013] Update multi language and UI styles

--- src/containers/Registration/RequestProcessing.js
+++ src/containers/Registration/RequestProcessing.js
@@ -7,27 +7,27 @@ const RequestProcessing = ({ createDate, deletedDate, assessedDate, approvedDate
 
     return (
         <>
-            <h5 className='content-page-header'>{t("Quá trình xử lý yêu cầu")}</h5>
+            <h5 className='content-page-header'>{t("RequestHistory")}</h5>
             <div className="box shadow request-processing">
                 <div className="row group">
-                <div className='col-xl-4'>
-                    {t("Thời gian gửi yêu cầu")}
-                    <div className="detail">{ createDate && moment(createDate, 'MM/DD/YYYY HH:mm').isValid() ? moment(createDate, 'MM/DD/YYYY HH:mm').format('DD/MM/YYYY | HH:mm:ss') : '' }</div>
-                </div>
-                <div className='col-xl-4'>
-                    {t("Thời gian thẩm định")}
-                    <div className="detail">{ assessedDate && moment(assessedDate, 'MM/DD/YYYY HH:mm').isValid() ? moment(assessedDate, 'MM/DD/YYYY HH:mm').format('DD/MM/YYYY | HH:mm:ss') : '' }</div>
-                </div>
-                <div className='col-xl-4'>
-                    {t("Thời gian phê duyệt")}
-                    <div className="detail">{ approvedDate && moment(approvedDate, 'MM/DD/YYYY HH:mm').isValid() ? moment(approvedDate, 'MM/DD/YYYY HH:mm').format('DD/MM/YYYY | HH:mm:ss') : '' }</div>
-                </div>
+                    <div className='col-xl-4'>
+                        {t("TimeToSendRequest")}
+                        <div className="detail">{ createDate ? moment(createDate).format('DD/MM/YYYY | HH:mm:ss') : '' }</div>
+                    </div>
+                    <div className='col-xl-4'>
+                        {t("ConsentDate")}
+                        <div className="detail">{ assessedDate ? moment(assessedDate).format('DD/MM/YYYY | HH:mm:ss') : '' }</div>
+                    </div>
+                    <div className='col-xl-4'>
+                        {t("ApprovalDate")}
+                        <div className="detail">{ approvedDate ? moment(approvedDate).format('DD/MM/YYYY | HH:mm:ss') : '' }</div>
+                    </div>
                 </div>
                 <div className="row group" style={{ marginTop: 10 }}>
-                <div className='col-xl-4'>
-                    {t('Thời gian hủy yêu cầu')}
-                    <div className="detail">{ deletedDate && moment(deletedDate, 'MM/DD/YYYY HH:mm').isValid() ? moment(deletedDate, 'MM/DD/YYYY HH:mm').format('DD/MM/YYYY | HH:mm:ss') : '' }</div>
-                </div>
+                    <div className='col-xl-4'>
+                        {t('CancelDate')}
+                        <div className="detail">{ deletedDate ? moment(deletedDate).format('DD/MM/YYYY | HH:mm:ss') : '' }</div>
+                    </div>
                 </div>
             </div>
         </>
