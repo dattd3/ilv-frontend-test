f82173911aee544c5eac89581bb075d5fcad033e
Tran Huu Dat <datth3@vingorup.net>
2023-05-12 10:02:58 +0700

    [config] Mở Tuyển dụng cho VinAI PROD

--- src/commons/Constants.js
+++ src/commons/Constants.js
@@ -219,7 +219,7 @@ const Constants = {
   },
   MODULE_COMPANY_AVAILABE: process.env.REACT_APP_ENVIRONMENT === 'PRODUCTION' ? 
   { //production todo: add  "V077", "V070" to TUYENDUNG
-    1: ["V040", "V005", "V079", "V041", "V030", "V077", "V070"],//TUYENDUNG + V061
+    1: ["V040", "V005", "V079", "V041", "V030", "V077", "V070", "V099"],//TUYENDUNG + V061
     2: ["V040", "V005", "V061", "V077"],//DANHGIA_TAIKI
     3: [],//DEXUATLUONG
     4: ["V040", "V077", "V070"],//NGHIVIEC
