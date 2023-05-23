634233084326db0f56e6622542868cdc8e588826
Nguyễn Văn Cường <cuongnv56@vingroup.net>
2023-05-23 12:04:07 +0700

    [ILVGR-1085] Update in-out history function

--- src/containers/TimeKeepingHistory/index.js
+++ src/containers/TimeKeepingHistory/index.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from "react";
+import DatePicker, {registerLocale } from 'react-datepicker'
 import axios from "axios";
 import { useTranslation } from "react-i18next";
 import { getRequestConfigs } from "commons/commonFunctions";
 import TimeKeepingList from "./TimeKeepingList";
 import LoadingModal from "components/Common/LoadingModal";
+import IconDatePicker from 'assets/img/icon/Icon_DatePicker.svg'
+import vi from 'date-fns/locale/vi'
+import 'react-datepicker/dist/react-datepicker.css'
+import moment from "../../../node_modules/moment/moment";
+registerLocale("vi", vi)
 
 const timeKeepingHistoryEndpoint = `${process.env.REACT_APP_REQUEST_URL}notifications/in/out/listbydate`;
 const APIConfig = getRequestConfigs();
@@ -26,7 +32,7 @@ export default function TimeKeepingHistory() {
         params: {
           companyCode: localStorage.getItem("companyCode"),
           culture: lang === "vi-VN" ? "vi" : "en",
-          date: date,
+          date: date && moment(date).isValid() ? moment(date).format('YYYY-MM-DD') : null,
           // page: 1,
           // pageSize: 1000,
         },
@@ -39,11 +45,39 @@ export default function TimeKeepingHistory() {
     }
   };
 
+  const handleDateInputChange = e => {
+    setDate(e)
+  }
+
+  const search = () => {
+    fetchData()
+  }
+
   return (
     <>
     <LoadingModal show={isLoading} />
     <div className="time-keeping-page">
       <div className="page-title">{t("timekeeping_history").toUpperCase()}</div>
+      <div className="container-card search-block">
+        <div className="row align-items-end">
+          <div className="col-md-4">
+            <label>{ t("SearchByDate") }</label>
+            <label className="date-input">
+              <DatePicker 
+                selected={date} 
+                maxDate={new Date()}
+                onChange={handleDateInputChange}
+                dateFormat="dd/MM/yyyy"
+                locale="vi"
+                className="form-control input" />
+                <span className="input-group-addon input-img"><img src={IconDatePicker} alt='IconDatePicker' /></span>
+              </label> 
+          </div>
+          <div className="col-md-4">
+            <button className="btn-search" onClick={search}>{t("Search")}</button>
+          </div>
+        </div>
+      </div>
       {timeKeepingData?.length > 0 ? (
         <div className="container-card">
           <TimeKeepingList apiResponseData={timeKeepingData} fromPage={true} />
