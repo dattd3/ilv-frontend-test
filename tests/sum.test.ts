7d95f19a16943b3985030608d9cc257cd1caa9b3
vuongvt2 <v.vuongvt2@vin3s.vn>
2023-06-08 15:52:00 +0700

    test cicd

--- tests/sum.test.ts
+++ tests/sum.test.ts
@@ -4,6 +4,6 @@ import sum from "./sum";
 
 describe('sum module', () => {
     test('add 1+2 to equal 3', () => {
-        expect(sum(1, 2)).toBe(4);
+        expect(sum(1, 2)).toBe(3);
     });
 });
