ff78afd39e9a3df546ee2a6a8f5360e9f134837a
Nguyễn Văn Cường <cuongnv56@vingroup.net>
2023-07-17 15:31:00 +0700

    Temp hide logo flippingbook

--- src/containers/HistoryVinGroup/index.js
+++ src/containers/HistoryVinGroup/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react"
+import $ from 'jquery'
 import { useTranslation } from "react-i18next"
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
@@ -15,44 +16,99 @@ function HistoryVinGroup(props) {
 
     useEffect(() => {
         setTimeout(() => { SetIsLoading(false) }, 800)
+
+        // const fetchIframeDate = () => {
+        //     window.fetch(
+        //         "https://online.flippingbook.com/view/211567232/1/", {
+        //         method: 'GET',
+        //         accept: 'text/html',
+        //         // mode: 'no-cors'
+        //         })
+        //         .then(async data => {
+        //             const data1 = await data.text();
+                    
+        //             console.log(data1)
+
+        //             var iframe = document.getElementById('myIframe');
+
+        //             iframe.contentWindow.document.open();
+        //             iframe.contentWindow.document.write(data1);
+        //             iframe.contentWindow.document.getElementsByClassName("logo-svg")[0]?.remove()
+        //             iframe.contentWindow.document.getElementsByClassName("logo-svg")[1]?.remove()
+        //             iframe.contentWindow.document.close();
+        //         });
+        // }
+
+        // fetchIframeDate()
+
         // props.handleFullScreen(true)
         const body = document.querySelector('#history-vingroup-page')
         body.scrollIntoView({
             behavior: 'smooth'
         }, 800)
 
+        // let curWin = window.top.document.getElementsByClassName('top-one')[0]
+
+        // var curWin = window.parent.document.getElementsByClassName('top-one')[0];
+        // curWin?.parentNode.removeChild(curWin);
+
+        // var x = window.document.getElementById("myIframe");
+        // var y=(x.contentWindow || x.contentDocument);
+        // $(y.document).find(".home-page")[0]?.remove();
+
+        // console.log(curWin)
+
+        // $(curWin).remove()
+
         // $(window).load(function(){
         //     $(window.parent).load(function(){
         //       $('#ifrm', window.parent.document).hide();
         //     });
         // });
+
+        // $('#myIframe').on('load', function() {
+        //     // console.log("Iframe loaded")
+        //     // setInterval(() => {
+        //     //     let a = $("#publication").parent().parent().parent().parent().find("#publication")
+        //     //     console.log(a?.length)
+        //     // }, 1000)
+
+        //     $(".home-page").remove()
+
+        //     // $(this).find(".home-page").remove()
+        // });
+
+   
+        // document.querySelectorAll('iframe').forEach(item => {
+        //     // var y = item.contentDocument.body.getElementsByTagName("iframe");
+        //     // setTimeout(() => {
+        //     //     $(y[0]).contents().find(".logo-svg").remove()
+        //     // }, [500])
+        // })
+
+        // window.addEventListener("message", (event) => {
+        //     console.log(event?.data)
+        //     // var iframe = document.getElementById("myIframe")
+        //     // var innerDoc = iframe?.contentDocument || iframe?.contentWindow?.document
+        //     // innerDoc.getElementsByClassName("home-page")[0]?.remove()
+        // });
     }, [])
 
-    useEffect(() => {
-        // $(function () {
-        //     setInterval(() => {
-        //         console.log(111)
-        //         $("#ifrm .logo-link").hide()
-        //     }, 1000)
-        // });
- 
-        // $("iframe .logo-link").hide()
-
-        // $(".logo-link").css("display", "none")
-
-        // $(".logo-link").css("display", "none");
-
-        // $(document).on('click','.hide-coppy-right',function(){
-        //     $(".logo-link").css("display", "none")
-        //     $("iframe .logo-link").hide()
-        // });
-    })
+    const handelIframe = () => {
+        // const iframe = document.getElementById("myIframe")
+        // let innerDoc = iframe.contentDocument || iframe.contentWindow.document
+        // innerDoc.getElementsByClassName("scroll-custom")[0]?.remove()
+        // innerDoc.getElementsByClassName("home-page")[0].remove()
+        // innerDoc.getElementById("main-content").remove()
+    }
 
     return (
         <>
             <LoadingModal show={isLoading} />
             <div className="history-vingroup-page" id="history-vingroup-page">
-                <iframe src="https://online.flippingbook.com/view/211567232/1/" allowFullScreen={true} id="ifrm"></iframe>
+                <div className="hide-logo"></div>
+                <iframe id="myIframe" src="https://online.flippingbook.com/view/211567232/1/" allowFullScreen={true} className="myIframe"></iframe>
+                {/* <iframe id="myIframe" allowFullScreen={true} src="http://localhost:3000/" className="myIframe" onLoad={handelIframe}></iframe> */}
             </div>
         </>
     )
