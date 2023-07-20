5a471d8a0545028de8eb489da8f91565d4ad64c7
dattd3 <dattd3@vingroup.net>
2023-07-20 12:18:05 +0700

    [ILVGR-1272][version history] create feature version history

--- src/containers/Instruct/index.js
+++ src/containers/Instruct/index.js
@@ -1,10 +1,34 @@
+import _ from "lodash";
 import React from "react";
 import axios from 'axios';
 import { withTranslation } from 'react-i18next';
 import iconPhone from '../../assets/img/phone.PNG';
 import iconGlobal from '../../assets/img/global.PNG';
-import _ from "lodash";
+import iconVersionHistory from '../../assets/img/icon/ic_version_history.svg';
 import HOCComponent from '../../components/Common/HOCComponent'
+
+const dataDummies = [
+   { 
+      title: 'Phiên bản 3.0.5',
+      desc: '- Thêm tính năng cập nhật quá trình công tác ngoài Tập đoàn cho VinES \n- Sửa lỗi hệ thống'
+   },
+   { 
+      title: 'Phiên bản 3.0.4',
+      desc: '- Thêm tính năng cập nhật quá trình công tác ngoài Tập đoàn cho VinES'
+   },
+   { 
+      title: 'Phiên bản 3.0.3',
+      desc: '- Thêm tính năng cập nhật quá trình công tác ngoài Tập đoàn cho VinES \n- Sửa lỗi hệ thống \n- Cải tiến tính năng đăng ký chấm dứt HĐLĐ \n- Thêm tính năng cập nhật quá trình công tác ngoài Tập đoàn cho VinES \n- Sửa lỗi hệ thống - Cải tiến tính năng đăng ký chấm dứt HĐLĐ'
+   },
+   { 
+      title: 'Phiên bản 3.0.2',
+      desc: '- Thêm tính năng cập nhật quá trình công tác ngoài Tập đoàn cho VinES \n- Sửa lỗi hệ thống'
+   },
+   { 
+      title: 'Phiên bản 3.0.1',
+      desc: '- Thêm tính năng cập nhật quá trình công tác ngoài Tập đoàn cho VinES'
+   },
+];
 
 class Instruct extends React.Component {
     constructor(props) {
@@ -114,6 +138,20 @@ class Instruct extends React.Component {
                         </div>
                      </div>
                   </div>
+               </div>
+
+               <h1 className="content-page-header">{t('instruct_version_history')}</h1>
+               <div className="card  p-3 version_histories">
+                  {dataDummies.map((ele, i) => (
+                     <div className="content-body" key={i}>
+                        <img className="icon" src={iconVersionHistory} />
+
+                        <div className="content">
+                           <div className="ttl"> {ele.title}</div>
+                           <div className="desc">{ele.desc}</div>
+                        </div>
+                     </div>
+                  ))}
                </div>
             </div>
         </>
