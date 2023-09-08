4b8352a12300e3664c4f950241a9bb68db9239fc
Tran Huu Dat <datth3@vingorup.net>
2023-09-08 15:34:32 +0700

    [ILVG-1371] update luồng phê duyệt yêu cầu bhxh

--- src/containers/Registration/ConfirmationModal.js
+++ src/containers/Registration/ConfirmationModal.js
@@ -104,7 +104,7 @@ class ConfirmationModal extends React.Component {
     getHostByRequestTypeId = (dataToSap) => {
         const requestTypeId = dataToSap?.[0]?.requestTypeId || dataToSap?.requestTypeId || "";
 
-        return !!requestTypeId && [12, 14, 15].includes(requestTypeId)
+        return !!requestTypeId && [12, 14, 15, Constants.INSURANCE_SOCIAL].includes(requestTypeId)
           ? process.env.REACT_APP_REQUEST_SERVICE_URL
           : process.env.REACT_APP_REQUEST_URL;
     }
