5fe3fb66add3430248ba09b03b0c35ecabf0ce6f
Nguyễn Mạnh Hiếu (CTVIS-VHPM-PSAPTH) <hieunm25@vingroup.net>
2023-09-13 16:05:24 +0700

    translate payment object in benefit refund request

--- src/containers/Welfare/InternalPayment/PaymentData.ts
+++ src/containers/Welfare/InternalPayment/PaymentData.ts
@@ -2,11 +2,11 @@ import { IDropdownValue } from "models/CommonModel";
 
 const PAYMENT_OBJECT = [
     {value: 1, labelVi: 'Bản thân', labelEn: 'Self'},
-    {value: 2, labelVi: 'Người thân', labelEn: 'Families'},
+    {value: 2, labelVi: 'Người thân', labelEn: 'Relatives'},
 ]
 
 export const getPaymentObjects = () : IDropdownValue[] => {
-    const locale = localStorage.getItem('locale') == 'vi-Vn' ? 'Vi' : 'En';
+    const locale = localStorage.getItem('locale') === 'vi-VN' ? 'Vi' : 'En';
     return PAYMENT_OBJECT.map(item => {
         return {
             value: item.value,
