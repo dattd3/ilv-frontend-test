143c87318075f3acafb399e6f023dcdffbfd4088
Tran Huu Dat <datth3@vingorup.net>
2023-09-30 14:15:50 +0700

    thêm logic check thay đôi data

--- src/containers/Welfare/InsuranceSocialContribute/SocialContributeData.ts
+++ src/containers/Welfare/InsuranceSocialContribute/SocialContributeData.ts
@@ -11,3 +11,32 @@ export const socialNumberType: IDropdownValue[] = [
         code: ''
     }
 ]
+
+export const RELATIONSHIP_WITH_INSURED = [
+    { value: 'V000', label: 'Bản thân' },
+    { value: 'V001', label: 'Cha ruột' },
+    { value: 'V002', label: 'Mẹ ruột' },
+    { value: 'V005', label: 'Vợ' },
+    { value: 'V006', label: 'Cha chồng' },
+    { value: 'V007', label: 'Mẹ chồng' },
+    { value: 'V008', label: 'Cha vợ' },
+    { value: 'V009', label: 'Mẹ vợ' },
+    { value: 'V013', label: 'Chồng' },
+    { value: 'V014', label: 'Con trai' },
+    { value: 'V015', label: 'Con gái' },
+];
+export const GENDER_LIST = [
+    {value: '1', label: 'Nam'},
+    {value: '2', label: 'Nữ'},
+]
+export const ROLE_TYPE = [
+    {value: '1', label: 'Chủ hộ'},
+    {value: '2', label: 'Thành viên'},
+]
+
+export const STATUS = {
+    OLD: 0,
+    NEW: 1,
+    UPDATE: 2,
+    DELETE: 3
+}
--- src/containers/Welfare/WelfareComponents/SocialContributeInfo.tsx
+++ src/containers/Welfare/WelfareComponents/SocialContributeInfo.tsx
@@ -12,12 +12,73 @@ import Download from "../../../assets/img/icon/ic_download_blue.svg";
 import CustomPaging from "../../../components/Common/CustomPaging";
 import CreateSocialContributeInfo from "../InsuranceSocialContribute/CreateSocialContributeInfo";
 import { IMemberInfo, ISocialContributeModel } from "models/welfare/SocialContributeModel";
+import { STATUS } from "../InsuranceSocialContribute/SocialContributeData";
 
 const SocialContributeInfo = (props: any) => {
         const { t } = props;
+        const [isCreateMode, setIsCreateMode] = useState(false);
         const [oldData, setOldData] = useState<ISocialContributeModel>({});
-        const [data, setData] = useState<ISocialContributeModel>({});
-        const [members, setMembers] = useState<IMemberInfo[]>([]);
+        const [data, setData] = useState<ISocialContributeModel>({
+            "socialNumberType": {
+                "value": "1",
+                "label": "Đề nghị cấp sổ"
+            },
+            "facilityRegisterName": {
+                "value": "O1",
+                "label": "Ốm thông thường"
+            },
+            "houseHoldNumber": "aaaaa",
+            "province": {
+                "value": "89",
+                "label": "An Giang"
+            },
+            "district": {
+                "value": "8903",
+                "label": "Huyện An Phú"
+            },
+            "ward": {
+                "value": "890314",
+                "label": "Thị Trấn Long Bình"
+            },
+            "note": "noi dung"
+        });
+        const [oldMembers, setOldMembers] = useState<IMemberInfo[]>([]);
+        const [members, setMembers] = useState<IMemberInfo[]>([
+            {
+                "relation": {
+                    "value": "V002",
+                    "label": "Mẹ ruột"
+                },
+                "fullName": "444",
+                "sex": {
+                    "value": "2",
+                    "label": "Nữ"
+                },
+                "birthDate": "28/09/2023",
+                "identityId": "778",
+                "type": {
+                    "value": "1",
+                    "label": "Chủ hộ"
+                }
+            },
+            {
+                "relation": {
+                    "value": "V006",
+                    "label": "Cha chồng"
+                },
+                "fullName": "rrrr",
+                "sex": {
+                    "value": "1",
+                    "label": "Nam"
+                },
+                "birthDate": "24/09/2023",
+                "identityId": "234234",
+                "type": {
+                    "value": "2",
+                    "label": "Thành viên"
+                }
+            }
+        ]);
         const [supervisors, setSupervisors] = useState<any[]>([])
         const [approver, setApprover] = useState<any>();
         const [loading, setLoading] = useState(false);
@@ -29,13 +90,9 @@ const SocialContributeInfo = (props: any) => {
         const [files, setFiles] = useState<any[]>([]);
 
         const checkDataChange = () => {
-            const STATUS = {
-                NEW: 1,
-                UPDATE: 2,
-                DELETE: 3
-            }
             const change = {};
-            const keyDropDown = ['socialNumberType', 'province', 'district', 'ward'];
+            const memberChange : any[] = [];
+            const keyDropDown = ['socialNumberType', 'province', 'district', 'ward', 'facilityRegisterName'];
             
             Object.keys(data).forEach(key => {
                 if(data[key] && !oldData[key]) {
@@ -43,7 +100,7 @@ const SocialContributeInfo = (props: any) => {
                 } else if (!data[key] && oldData[key]) {
                     change[key] = STATUS.DELETE
                 } else if(data[key] && keyDropDown.includes(key)){
-                    if(oldData[key].value != data[key].value || oldData[key].label != data[key].label) {
+                    if(oldData[key].value != data[key].value || oldData[key].label != data[key].label || oldData[key].code != data[key].code) {
                         change[key] = STATUS.UPDATE;
                     }
                 } else if(data[key] && !keyDropDown.includes(key)) {
@@ -55,13 +112,63 @@ const SocialContributeInfo = (props: any) => {
                     change[key +'_value'] = data[key];
                 }
             });
+            members.map((mem, index) => {
+                if(mem.status == STATUS.OLD) {
+                    memberChange.push({status: STATUS.OLD});
+                }
+                if(mem.status == STATUS.NEW) {
+                    memberChange.push({status: STATUS.NEW, value: mem});
+                }
+                if(mem.status == STATUS.DELETE) {
+                    memberChange.push({status: STATUS.DELETE});
+                }
+                if(mem.status == STATUS.UPDATE) {
+                    let _newMem = {};
+                    Object.keys(mem).map(key => {
+                        if(key == 'status') return;
+                        if(typeof mem[key] == 'string' && mem[key] != oldMembers[index][key]) {
+                            _newMem[key] = mem[key];
+                        }
+                        if(typeof mem[key] == 'object' && mem[key].value != oldMembers[index][key].value) {
+                            _newMem[key] = mem[key];
+                        }
+                    });
+                    memberChange.push({status: STATUS.UPDATE, value: _newMem});
+                }
+            })
+
+            console.log('change>>>', change, memberChange);
         }
 
+        const onEdit = () => {
+            let newEditable = !isCreateMode;
+            if(newEditable) {
+                setOldData({
+                    ...data,
+                })
+                let _oldmember = members.map(mem => {
+                    return {
+                        ...mem,
+                        status: STATUS.OLD
+                    };
+                })
+                setOldMembers(_oldmember);
+            }
+            setIsCreateMode(newEditable);
+        }
+
+        const onSubmit = () => {
+            console.log('data>>', data);
+            console.log('members>>>', members);
+            checkDataChange();
+        }
         return (<>
             <div className="health-info-page">
                 <div className="clearfix edit-button w-100 pb-2">
                     <a href="/insurance-manager/social-contribute-info"><div className="btn bg-white btn-create"
                     ><i className="fas fa-plus"></i> {t('createRequest')}</div></a>
+                    <a onClick={() => onEdit()}><div className="btn bg-white btn-create"
+                    ><i className="fas fa-plus"></i> {t('Edit')}</div></a>
                 </div>
                 <CreateSocialContributeInfo
                     t={t}
@@ -76,7 +183,8 @@ const SocialContributeInfo = (props: any) => {
                     removeFile={()=>{}}
                     members = {members}
                     setMembers={setMembers}
-                    isCreateMode = {true}
+                    onSubmit={onSubmit}
+                    isCreateMode = {isCreateMode}
                 />
             </div>
         </>)
