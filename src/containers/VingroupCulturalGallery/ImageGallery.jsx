f0352338c70d1afb84a97b7fd9819cce74d55deb
Nguyễn Mạnh Hiếu (CTVIS-VHPM-PSAPTH) <hieunm25@vingroup.net>
2023-10-09 10:07:45 +0700

    fix: update styles image gallery

--- src/containers/VingroupCulturalGallery/ImageGallery.jsx
+++ src/containers/VingroupCulturalGallery/ImageGallery.jsx
@@ -1,33 +1,109 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const ImageGallery = ({ data }) => {
-  const [indexZoomIn, setIndexZoomIn] = useState(-1);
-  const handleZoomInImage = (index) => {
-    setIndexZoomIn(index);
+  const [idZoomIn, setIdZoomIn] = useState(-1);
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [lastScrollPosition, setLastScrollPosition] = useState(0);
+
+  useEffect(() => {
+    document.getElementById("content-wrapper").addEventListener("scroll", handleScroll, false);
+    return () => document.getElementById("content-wrapper").removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleScroll = () => {
+    if (idZoomIn === -1) {
+      setScrollPosition(document.getElementById("content-wrapper").scrollTop);
+    }
   };
 
+  useEffect(() => {
+    if (idZoomIn === -1) {
+      scrollToLastPosition();
+    }
+  }, [idZoomIn]);
+
+  const scrollToLastPosition = () => {
+    document.getElementById("content-wrapper").scrollTo({
+      left: 0,
+      top: lastScrollPosition,
+      behavior: "instant"
+    });
+  };
+
+  const handleZoomInImage = (index) => {
+    if (index > 0) setLastScrollPosition(scrollPosition);
+    setIdZoomIn(index);
+  };
+
+  const length = data.length,
+    countItem = Math.floor(data.length / 3);
+  let col1 = [],
+    col2 = [],
+    col3 = [];
+
+  switch (length % 3) {
+    case 1:
+      col1 = data.slice(0, countItem + 1);
+      col2 = data.slice(countItem + 1, countItem * 2);
+      col3 = data.slice(countItem * 2, length);
+      break;
+    case 2:
+      col1 = data.slice(0, countItem + 1);
+      col2 = data.slice(countItem + 1, countItem * 2 + 1);
+      col3 = data.slice(countItem * 2 + 1, length);
+      break;
+    case 0:
+    default:
+      col1 = data.slice(0, countItem);
+      col2 = data.slice(countItem, countItem * 2);
+      col3 = data.slice(countItem * 2, length);
+      break;
+  }
+
   return (
-    <div className="image-gallery">
-      {indexZoomIn > -1 && (
-        <img
-          alt=""
-          src={data[indexZoomIn].link}
-          className="zoomed-in-img"
-          onClick={() => handleZoomInImage(-1)}
-        />
-      )}
-      {data.map(
-        (img, index) => (
-          <img
-            style={{ display: indexZoomIn > -1 ? "none" : "block" }}
-            key={img.id}
-            src={img.link}
-            alt=""
-            onClick={() => handleZoomInImage(index)}
-            className="image"
-          />
-        )
-      )}
+    <div className="image-gallery" style={idZoomIn > -1 ? { display: "flex", justifyContent: "center" } : {}}>
+      <div className="col-image">
+        {col1.map(
+          (img) => (
+            <img
+              style={{ display: (idZoomIn > -1 && idZoomIn !== img.id) ? "none" : "block" }}
+              key={img.id}
+              src={img.link}
+              alt=""
+              onClick={() => handleZoomInImage(idZoomIn === img.id ? - 1 : img.id)}
+              className={(idZoomIn === img.id) ? "zoomed-in-img" : "image"}
+            />
+          )
+        )}
+      </div>
+      <div className="col-image">
+        {col2.map(
+          (img) => (
+            <img
+              style={{ display: (idZoomIn > -1 && idZoomIn !== img.id) ? "none" : "block" }}
+              key={img.id}
+              src={img.link}
+              alt=""
+              onClick={() => handleZoomInImage(idZoomIn === img.id ? - 1 : img.id)}
+              className={(idZoomIn === img.id) ? "zoomed-in-img" : "image"}
+            />
+          )
+        )}
+      </div>
+      <div className="col-image">
+        {col3.map(
+          (img) => (
+            <img
+              style={{ display: (idZoomIn > -1 && idZoomIn !== img.id) ? "none" : "block" }}
+              key={img.id}
+              src={img.link}
+              alt=""
+              onClick={() => handleZoomInImage(idZoomIn === img.id ? - 1 : img.id)}
+              className={(idZoomIn === img.id) ? "zoomed-in-img" : "image"}
+            />
+          )
+        )}
+      </div>
     </div>
   );
 };
