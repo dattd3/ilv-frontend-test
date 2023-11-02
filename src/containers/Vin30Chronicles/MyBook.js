6599dc0d07cf2855c5c6584181e883612d9681ad
Nguyễn Văn Cường <cuongnv56@vingroup.net>
2023-11-02 11:07:22 +0700

    Temp fix zoom Sử ký

--- src/containers/Vin30Chronicles/MyBook.js
+++ src/containers/Vin30Chronicles/MyBook.js
@@ -876,7 +876,7 @@ export default function MyBook(props) {
                       drawShadow={false}
                       mobileScrollSupport={false}
                       onFlip={handleFlip}
-                      // useMouseEvents={false}
+                      useMouseEvents={false}
                       ref={bookRef}
                     >
                       {pages.map((item) => (
