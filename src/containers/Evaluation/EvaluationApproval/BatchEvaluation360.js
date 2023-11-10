1b5a4542721d9b6415693cf0ec8b2eccce1db5d3
Nguyễn Văn Cường <cuongnv56@vingroup.net>
2023-11-10 17:59:36 +0700

    [ILVGR-1501] Ghép api Cải tiến tính năng đánh giá 360

--- src/containers/Evaluation/EvaluationApproval/BatchEvaluation360.js
+++ src/containers/Evaluation/EvaluationApproval/BatchEvaluation360.js
@@ -25,6 +25,7 @@ import IconSend from '../../../assets/img/icon/Icon_send.svg'
 import IconDatePicker from 'assets/img/icon/Icon_DatePicker.svg'
 import 'react-datepicker/dist/react-datepicker.css'
 import vi from 'date-fns/locale/vi'
+import { hasNotValue } from "../Utils"
 registerLocale("vi", vi)
 
 const employeeCode = localStorage.getItem('employeeNo')
@@ -45,6 +46,8 @@ const BatchEvaluation360 = ({ evaluationData }) => {
         totalPages: 1,
     })
     const [refresh, SetRefresh] = useState(false)
+    const [statusModal, SetStatusModal] = useState({ isShow: false, isSuccess: true, content: "", needReload: true })
+    const [errors, SetErrors] = useState({})
 
     useEffect(() => {
         const indexLastItem = paging.pageIndex * paging.pageSize
@@ -61,8 +64,9 @@ const BatchEvaluation360 = ({ evaluationData }) => {
     useEffect(() => {
         const indexLastItem = paging.pageIndex * paging.pageSize
         const indexFirstItem = indexLastItem - paging.pageSize
+
         SetUsers((evaluationDataState?.data || []).slice(indexFirstItem, indexLastItem))
-    }, [paging.pageIndex])
+    }, [paging])
 
     useEffect(() => {
         SetPaging({
@@ -71,40 +75,131 @@ const BatchEvaluation360 = ({ evaluationData }) => {
         })
     }, [paging.pageSize])
 
-    const handleInputChange = (index, targetIndex, e) => {
+    const handleScoreChange = (index, targetIndex, e) => {
         const evaluationDataStateClone = {...evaluationDataState}
         evaluationDataStateClone.data[index].listGroup[1].listTarget[targetIndex].seftPoint = e?.target?.value || null
+        if ((evaluationDataStateClone.data[index].listGroup[1].listTarget || []).every(item => !hasNotValue(item?.seftPoint))) {
+            evaluationDataStateClone.data[index].status = evaluation360Status.evaluated
+        }
         SetEvaluationDataState(evaluationDataStateClone)
     }
 
-    const handleChangePageSize = () => {
-
+    const handleInputChange = (index, e) => {
+        const evaluationDataStateClone = {...evaluationDataState}
+        evaluationDataStateClone.data[index].opinion = e?.target?.value || ''
+        SetEvaluationDataState(evaluationDataStateClone)
     }
 
-    const handleChangePage = () => {
+    const handleChangePageSize = (size = 1) => {
+        SetPaging({
+            ...paging,
+            pageSize: size,
+        })
+    }
 
+    const handleChangePage = (page = 1) => {
+        SetPaging({
+            ...paging,
+            pageIndex: page,
+        })
+    }
+
+    const onHideStatusModal = () => {
+        SetStatusModal({
+            isShow: false,
+            isSuccess: true,
+            content: '',
+            needReload: true,
+        })
+    
+        if (statusModal?.needReload) {
+            window.location.reload()
+        }
+    }
+
+    const isDataValid = () => {
+        const errorTemp = {}
+        for (let index = 0, len = evaluationDataState?.data?.length; index < len; index++) {
+            const element = evaluationDataState?.data[index]
+            if (!hasNotValue(element?.opinion) 
+            || (element?.listGroup?.[1]?.listTarget || []).some(item => item?.seftPoint !== '' && item?.seftPoint !== null && item?.seftPoint !== undefined)) { // Đã thực hiện đánh giá
+                for (let targetIndex = 0, lenTargets = element?.listGroup?.[1]?.listTarget?.length ; targetIndex < lenTargets; targetIndex++) {
+                    const target = element?.listGroup?.[1]?.listTarget[targetIndex]
+                    if (hasNotValue(target?.seftPoint)) {
+                        errorTemp[`${index}-${targetIndex}-seftPoint`] = t("Required")
+                    }
+                }
+            }
+        }
+        SetErrors(errorTemp)
+        const hasError = (Object.values(errorTemp) || []).some(item => item)
+        if (hasError) {
+          SetStatusModal({
+            ...statusModal,
+            isShow: true,
+            isSuccess: false,
+            content: t("RequiredEvaluationScore"),
+            needReload: false,
+          })
+        }
+    
+        return !hasError
+    }
+
+    const sendEvaluation = async (action = actionButton.save) => {
+        if (action == actionButton.approve) {
+            const isValid = isDataValid()
+            if (!isValid) {
+                return
+            }
+        }
+
+        const statusModalTemp = { ...statusModal }
+        SetIsLoading(true)
+        try {
+            const config = getRequestConfigurations()
+            // return
+            const response = await axios.post(`${process.env.REACT_APP_HRDX_PMS_URL}api/${evaluationApiVersion.v1}/targetform/update-batch`, { requestString: JSON.stringify(evaluationDataState.data || []) }, config)
+            SetErrors({})
+            statusModalTemp.isShow = true
+            statusModalTemp.isSuccess = false
+            statusModalTemp.content = t("EvaluationFailedToEvaluateForm")
+            if (response?.data) {
+                const result = response.data?.result
+                if (result?.code == Constants.PMS_API_SUCCESS_CODE) {
+                    statusModalTemp.isSuccess = true
+                    statusModalTemp.content = t("EvaluationFormEvaluatedSuccessfully")
+                    statusModalTemp.needReload = true
+                } else {
+                    statusModalTemp.content = result?.message
+                }
+            }
+            SetStatusModal(statusModalTemp)
+        } catch (e) {
+            statusModalTemp.isShow = true
+            statusModalTemp.isSuccess = false
+            statusModalTemp.content = t("AnErrorOccurred")
+            statusModalTemp.needReload = false
+            SetStatusModal(statusModalTemp)
+        } finally {
+            SetIsLoading(false)
+        }
     }
 
     const lang = getCurrentLanguage()
     const scores = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
     const jobPerformanceGroup = evaluationDataState?.data?.[0]?.listGroup?.[1]
-
-    console.log('ahihihi => ', jobPerformanceGroup)
-    console.log('evaluationDataState => ', evaluationDataState)
-    console.log('users => ', users)
 
     return (
         <>
         <LoadingModal show={isLoading} />
-        {/* <StatusModal show={statusModal.isShow} isSuccess={statusModal.isSuccess} content={statusModal.content} onHide={onHideStatusModal} className='evaluation-status-modal' /> */}
-        
+        <StatusModal show={statusModal.isShow} isSuccess={statusModal.isSuccess} content={statusModal.content} onHide={onHideStatusModal} className='evaluation-status-modal' />
         <div className="card shadow batch-evaluation-360">
             <div className="wrap-table-batch-evaluation-360">
                 <table>
                     <thead>
                         <tr>
-                            <th className="col-customize sticky-col kpi">Nội dung đánh giá</th>
+                            <th className="col-customize sticky-col kpi">{t("ContentsOfAssessment")}</th>
                             {
                                 (users || []).map((user, uIndex) => {
                                     return (
@@ -143,14 +238,23 @@ const BatchEvaluation360 = ({ evaluationData }) => {
                                                 (users || []).map((user, uIndex) => {
                                                     return (
                                                         <td key={`${target?.id}-${uIndex}-${paging.pageIndex}`} className="col-customize user-point">
-                                                            <select onChange={e => handleInputChange(uIndex, tIndex, e)} value={user?.listGroup?.[1]?.listTarget?.[tIndex]?.seftPoint || ''}>
-                                                                <option value=''>{t("EvaluationDetailPartSelectScore")}</option>
-                                                                {
-                                                                    (scores || []).map((score, i) => {
-                                                                        return (<option value={score} key={i}>{score}</option>)
-                                                                    })
-                                                                }
-                                                            </select>
+                                                            {
+                                                                !user?.isEdit || user?.isDeleted || user?.status == evaluation360Status.completed 
+                                                                ? (
+                                                                    <input type="text" value={parseInt(user?.listGroup?.[1]?.listTarget?.[tIndex]?.seftPoint) === 0 ? 'N/A' : (user?.listGroup?.[1]?.listTarget?.[tIndex]?.seftPoint ?? '')} disabled />
+                                                                )
+                                                                : (
+                                                                    <select onChange={e => handleScoreChange(uIndex, tIndex, e)} value={user?.listGroup?.[1]?.listTarget?.[tIndex]?.seftPoint || ''}>
+                                                                        <option value=''>{t("EvaluationDetailPartSelectScore")}</option>
+                                                                        {
+                                                                            (scores || []).map((score, i) => {
+                                                                                return (<option value={score} key={i}>{score === 0 ? 'N/A' : score}</option>)
+                                                                            })
+                                                                        }
+                                                                    </select>
+                                                                )
+                                                            }
+                                                            { errors[`${uIndex}-${tIndex}-seftPoint`] && (<div className="error-message">{errors[`${uIndex}-${tIndex}-seftPoint`]}</div>) }
                                                         </td>
                                                     )
                                                 })
@@ -168,7 +272,7 @@ const BatchEvaluation360 = ({ evaluationData }) => {
                                         <td colSpan={users?.length} className="row-seperate"></td>
                                     </tr>
                                     <tr className="kpi-item">
-                                        <td className="col-customize sticky-col kpi">Ý kiến</td>
+                                        <td className="col-customize sticky-col kpi">{t("Opinion")}</td>
                                         {
                                             (users || []).map((user, uIndex) => {
                                                 return (
@@ -177,8 +281,8 @@ const BatchEvaluation360 = ({ evaluationData }) => {
                                                             rows={3} 
                                                             placeholder={t("EvaluationInput")} 
                                                             value={user?.opinion || ""} 
-                                                            onChange={e => handleInputChange(groupIndex, groupChildrenLv1Index, kpiIndex, 'seftOpinion', e)} 
-                                                            // disabled={isDisableEmployeeComment} 
+                                                            onChange={e => handleInputChange(uIndex, e)} 
+                                                            disabled={!user?.isEdit || user?.isDeleted || user?.status == evaluation360Status.completed} 
                                                         />
                                                     </td>
                                                 )
@@ -451,11 +555,10 @@ const BatchEvaluation360 = ({ evaluationData }) => {
                     </tbody>
                 </table>
             </div>
-
             <div className="bottom-region">
                     <div className="customize-display">
                         <label>{t("EvaluationShow")}</label>
-                        <select value={paging.pageSize} onChange={(e) => handleChangePageSize('approval', e?.target?.value)}>
+                        <select value={paging.pageSize} onChange={(e) => handleChangePageSize(e?.target?.value)}>
                             {
                                 listPageSizes.map((page, i) => {
                                     return <option value={page} key={i}>{page}</option>
@@ -466,15 +569,16 @@ const BatchEvaluation360 = ({ evaluationData }) => {
                     <div className="paging-block">
                         <CustomPaging 
                             pageSize={parseInt(paging.pageSize)} 
-                            onChangePage={(page) => handleChangePage('approval', page)} 
+                            onChangePage={(page) => handleChangePage(page)} 
                             totalRecords={evaluationDataState?.total} 
                             needRefresh={refresh} 
                         />
                     </div>
                 </div>
             </div>
-            <div className="button-region">
-                <button className="btn-action approve"><img src={IconSend} alt="Approve" />Gửi</button>
+            <div className="button-region-batch-evaluation-360 d-flex justify-content-end">
+                <button className="btn-action save" onClick={() => sendEvaluation(actionButton.save)}><img src={IconSend} alt="Save" />{t("Save")}</button>
+                <button className="btn-action send" onClick={() => sendEvaluation(actionButton.approve)}><img src={IconSend} alt="Send" />{t("Evaluation360ButtonSend")}</button>
             </div>
         </>
     )
