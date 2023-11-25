340e7d93552db41e1054336ad1806000a6f3e18e
Nguyễn Văn Cường <cuongnv56@vingroup.net>
2023-11-25 12:05:57 +0700

    [ILVGR-1618] Xây tính năng thông báo chương trình EVoucher và xem Voucher của tôi (create UI)

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,12 +1,43 @@
-import React from "react"
+import { useState, useEffect } from "react"
+import axios from 'axios'
+import { getRequestConfigurations } from "commons/Utils"
 import NewsOnHome from './NewsOnHome'
 import HOCComponent from '../../components/Common/HOCComponent'
+import BannerModal from "./BannerModal"
  
 function Dashboard(props) {
+  const [isShow, setIsShow] = useState(true)
+  const [banners, setBanners] = useState([])
+
+  useEffect(() => {   
+    const fetchListBanners = async () => {
+      try {
+        const config = getRequestConfigurations()
+        const response = await axios.get(`${process.env.REACT_APP_REQUEST_URL}evoucher-vinhomes/popup`, config)
+        setBanners(response?.data?.data || [])
+      } finally {
+        // SetIsLoading(false)
+      }
+    }
+
+    fetchListBanners()
+  }, [])
+
+  const onHideModal = () => {
+    setIsShow(false)
+  }
+
   return (
-    <div className="home-page">
-      <NewsOnHome />
-    </div>
+    <>
+      <BannerModal 
+        isShow={isShow} 
+        banners={banners}
+        onHideModal={onHideModal}
+      />
+      <div className="home-page">
+        <NewsOnHome />
+      </div>
+    </>
   )
 }
 
