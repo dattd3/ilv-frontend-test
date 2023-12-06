90e973092ee92c2d441bc8e7d47a63e58cf19bc5
Nguyễn Văn Cường <cuongnv56@vingroup.net>
2023-12-06 15:06:32 +0700

    [ILVGR-1618] Xây tính năng thông báo chương trình EVoucher và xem Voucher của tôi (ghép api)

--- src/containers/MyVoucher/ModalQRCodes.js
+++ src/containers/MyVoucher/ModalQRCodes.js
@@ -1,9 +1,11 @@
 import { useState } from "react"
-import { Modal, Carousel } from "react-bootstrap";
+import { Modal, Carousel } from "react-bootstrap"
+import { useTranslation } from "react-i18next"
+import moment from 'moment'
 import IconClose from 'assets/img/icon/icon_x.svg'
-import moment from 'moment';
 
 const ModalQRCodes = ({ isShowModal, listQRs = [], onHideModal }) => {
+    const { t } = useTranslation()
     const [carouselControl, SetCarouselControl] = useState({
         showPrev: true,
         showNext: true,
@@ -19,40 +21,42 @@ const ModalQRCodes = ({ isShowModal, listQRs = [], onHideModal }) => {
                 <button className="btn-close" title='Close' onClick={onHideModal}><img src={IconClose} alt='Close' /></button>
                 <div className='content'>
                 {
-                    listQRs?.length > 0 && (
-                    <Carousel 
-                        interval={null} 
-                        indicators={false}
-                        onSlide={(eventKey) => {
-                            const carouselControlClone = {...carouselControl}
+                    listQRs?.length > 0 
+                    ? (
+                        <Carousel 
+                            interval={null} 
+                            indicators={false}
+                            onSlide={(eventKey) => {
+                                const carouselControlClone = {...carouselControl}
 
-                            SetCarouselControl(carouselControlClone)
-                        }}
-                        // prevLabel={""}
-                        // prevIcon={""}
-                        // nextIcon={""}
-                        // nextLabel={""}
-                        // controls={false}
-                    >
-                    {
-                        (listQRs || []).map((ele, i) => (
-                            <Carousel.Item key={i}>
-                                <div className="d-flex justify-content-center flex-column qr-item">
-                                    <div className="d-flex justify-content-center image">
-                                        <img
-                                            src={`data:image/png;base64,${ele?.qrCode}`}
-                                            alt="QR"
-                                            className='qr-image'
-                                        />
+                                SetCarouselControl(carouselControlClone)
+                            }}
+                            // prevLabel={""}
+                            // prevIcon={""}
+                            // nextIcon={""}
+                            // nextLabel={""}
+                            // controls={false}
+                        >
+                        {
+                            (listQRs || []).map((ele, i) => (
+                                <Carousel.Item key={i}>
+                                    <div className="d-flex justify-content-center flex-column qr-item">
+                                        <div className="d-flex justify-content-center image">
+                                            <img
+                                                src={`data:image/png;base64,${ele?.qrCode}`}
+                                                alt="QR"
+                                                className='qr-image'
+                                            />
+                                        </div>
+                                        <div className="d-flex justify-content-center code">{ele?.code}</div>
+                                        <div className="d-flex justify-content-center time">HSD: <span className="font-weight-bold">{moment(ele?.validFrom).format("DD/MM/YYYY")} - {moment(ele?.validTo).format("DD/MM/YYYY")}</span></div>
                                     </div>
-                                    <div className="d-flex justify-content-center code">{ele?.code}</div>
-                                    <div className="d-flex justify-content-center time">HSD: <span className="font-weight-bold">{moment(ele?.validFrom).format("DD/MM/YYYY")} - {moment(ele?.validTo).format("DD/MM/YYYY")}</span></div>
-                                </div>
-                            </Carousel.Item>
-                        ))
-                    }
-                    </Carousel>
+                                </Carousel.Item>
+                            ))
+                        }
+                        </Carousel>
                     )
+                    : (<div className="alert alert-danger data-not-found">{t("NoDataFound")}</div>)
                 }
                 </div>
             </Modal.Body>
--- src/containers/MyVoucher/VoucherTabContent.js
+++ src/containers/MyVoucher/VoucherTabContent.js
@@ -1,15 +1,19 @@
 import { useState, useEffect } from "react"
 import { useTranslation } from "react-i18next"
 import axios from 'axios'
-import { tabMapping, tabStatusMapping } from "."
+import { tabStatusMapping } from "."
 import { getRequestConfigurations } from '../../commons/Utils'
 import LoadingModal from 'components/Common/LoadingModal'
 import VoucherItem from "./VoucherItem"
+import CustomPaging from "components/Common/CustomPaging"
 
-const VoucherTabContent = ({ needLoadData, activeTab, vouchersSearched }) => {
+const VoucherTabContent = ({ needLoadData, activeTab }) => {
+    const listPageSizes = [10, 20, 30, 40, 50]
     const currentEmployeeCode = localStorage.getItem('employeeNo')
     const { t } = useTranslation()
     const [isLoading, SetIsLoading] = useState(false)
+    const [keyword, setKeyword] = useState('')
+    const [isSearching, setIsSearching] = useState(false)
     const [vouchers, setVouchers] = useState({
         total: 0,
         listVouchers: [],
@@ -18,52 +22,114 @@ const VoucherTabContent = ({ needLoadData, activeTab, vouchersSearched }) => {
         pageIndex: 1,
         pageSize: 10,
     })
+    const [refresh, SetRefresh] = useState(false)
+
+    const fetchListVouchers = async (pagingInput, _isSearching = false) => {
+        SetIsLoading(true)
+        try {
+            const config = getRequestConfigurations()
+            const payload = {
+                employeeCode: currentEmployeeCode,
+                voucherStatus: tabStatusMapping[activeTab],
+                pageIndex: pagingInput ? pagingInput?.pageIndex : paging.pageIndex,
+                pageSize: pagingInput ? pagingInput?.pageSize : paging.pageSize,
+                search: (_isSearching || isSearching) ? keyword?.trim() : '',
+            }
+            const response = await axios.post(`${process.env.REACT_APP_REQUEST_URL}evoucher-vinhomes/3rd/stalls`, payload, config)
+            if (response?.data?.data?.result) {
+                setVouchers({
+                    total: response?.data?.data?.result?.totalRecords || 0,
+                    listVouchers: response?.data?.data?.result?.data || []
+                })
+            }
+        } finally {
+            SetIsLoading(false)
+        }
+    }
 
     useEffect(() => {
-        console.log("needLoadData ", needLoadData)
-        const fetchListVouchers = async () => {
-            SetIsLoading(true)
-            try {
-                const config = getRequestConfigurations()
-                const payload = {
-                    employeeCode: currentEmployeeCode,
-                    voucherStatus: tabStatusMapping[activeTab],
-                    pageIndex: paging.pageIndex,
-                    pageSize: paging.pageSize,
-                    search: "",
-                }
-                const response = await axios.post(`${process.env.REACT_APP_REQUEST_URL}evoucher-vinhomes/3rd/stalls`, payload, config)
-                if (response?.data?.data?.result) {
-                    setVouchers({
-                        total: response?.data?.data?.result?.totalRecords || 0,
-                        listVouchers: response?.data?.data?.result?.data || []
-                    })
-                }
-            } finally {
-                SetIsLoading(false)
-            }
-        }
-    
-        needLoadData && fetchListVouchers(activeTab)
+        setIsSearching(false)
+        setKeyword("")
+        needLoadData && fetchListVouchers()
     }, [needLoadData, activeTab])
 
-    const voucherInfos = vouchersSearched ? vouchersSearched : vouchers
+    const handleInputChange = e => {
+        setKeyword(e?.target?.value || '')
+    }
+
+    const handleSearch = async (e) => {
+        e.preventDefault()
+        setIsSearching(true)
+        const pagingInput = {
+            pageIndex: 1,
+            pageSize: paging.pageSize,
+        }
+        fetchListVouchers(pagingInput, true)
+    }
+
+    const handleChangePageSize = (pageSize) => {
+        const pagingInput = {
+            pageIndex: 1,
+            pageSize: pageSize,
+        }
+        setPaging(pagingInput)
+        fetchListVouchers(pagingInput, false)
+    }
+
+    const handleChangePage = (page) => {
+        const pagingInput = {
+            pageIndex: page,
+            pageSize: paging.pageSize,
+        }
+        setPaging(pagingInput)
+        fetchListVouchers(pagingInput)
+    }
 
     return (
         <>
             <LoadingModal show={isLoading} />
+            <div className="search-block">
+                <form className="d-flex" onSubmit={handleSearch}>
+                    <input type="text" className="search-input" placeholder="Tìm kiếm" value={keyword || ""} onChange={handleInputChange} />
+                    <button type="submit" className="btn-search">{t("Search")}</button>
+                </form>
+            </div>
             {
-                voucherInfos?.listVouchers?.length > 0
+                vouchers?.listVouchers?.length > 0
                 ? (
-                    (voucherInfos?.listVouchers || []).map((voucher, index) => {
-                        return (
-                            <VoucherItem
-                                key={`${activeTab}-${index}`}
-                                voucher={voucher}
-                                activeTab={activeTab}
-                            />
-                        )
-                    })
+                    <>
+                        {
+                            (vouchers?.listVouchers || []).map((voucher, index) => {
+                                return (
+                                    <VoucherItem
+                                        key={`${activeTab}-${index}`}
+                                        voucher={voucher}
+                                        activeTab={activeTab}
+                                    />
+                                )
+                            })
+                        }
+                        <div className="d-flex align-items-center paging-region">
+                            <div className="customize-display">
+                                <label>{t("EvaluationShow")}</label>
+                                <select value={paging.pageSize || listPageSizes[0]} onChange={(e) => handleChangePageSize(e?.target?.value)}>
+                                    {
+                                        listPageSizes.map((page, i) => {
+                                            return <option value={page} key={i}>{page}</option>
+                                        })
+                                    }
+                                </select>
+                            </div>
+                            <div className="paging-block">
+                                <CustomPaging 
+                                    pageSize={parseInt(paging.pageSize)} 
+                                    onChangePage={page => handleChangePage(page)} 
+                                    totalRecords={vouchers?.total} 
+                                    needRefresh={refresh} 
+                                />
+                            </div>
+                        </div>
+                    </>
                 )
                 : (<div className="alert alert-danger data-not-found">{t("NoDataFound")}</div>)
             }
